fac6fd6a5623bc308605c51e85df3edacd1ab710
bjornstahl@34ee8734-9d04-4b2f-9570-a8dab4294b71 <bjornstahl@34ee8734-9d04-4b2f-9570-a8dab4294b71>
2012-02-29 00:39:53 +0000

    backup commit, some fixes to keyconf.
    FossilOrigin-Name: 899efe032c73887e9b7413534589f81df7f8ec5d98d6d2671debf9cb770f6757

--- resources/scripts/keyconf.lua
+++ resources/scripts/keyconf.lua
@@ -36,14 +36,62 @@ local default_player_group = {
 	"rRIGHT",
 	"rSTART",
 	" SELECT",
-    "rCOIN1",
-	" BUTTON1",
-	" BUTTON2",
-	" BUTTON3",
-	" BUTTON4",
-	" BUTTON5",
-	" BUTTON6",
+    "rCOIN1"
 };
+
+local function keyconf_color(self, borderr, borderg, borderb, windowr, windowg, windowb)
+	local bprops = {x = 0, y = 0, width = 1, height = 1, opa = 1.0};
+	local wprops = {opa = 1.0, width = 1, height = 1, x = 3, y = 3};
+-- copy surface properties so that we can reset them on the new objects
+	if (self.window and self.window ~= BADID) then wprops = image_surface_properties(self.window); delete_image(self.window); end
+	if (self.border and self.border ~= BADID) then bprops = image_surface_properties(self.border); delete_image(self.border); end
+
+	self.border = fill_surface(8, 8, borderr, borderg, borderb);
+	self.window = fill_surface(8, 8, windowr, windowg, windowb);
+
+-- we associate with an anchor used for movement so that we can clip to
+-- window rather than border
+	link_image(self.border, self.anchor);
+	link_image(self.window, self.anchor);
+	
+	image_mask_clear(self.border, MASK_SCALE);
+	image_mask_clear(self.border, MASK_OPACITY);
+	image_mask_clear(self.window, MASK_SCALE);
+	image_mask_clear(self.window, MASK_OPACITY);
+
+-- reset all the old properties
+	resize_image(self.border, bprops.width, bprops.height);
+	resize_image(self.window, wprops.width, wprops.height);
+	move_image(self.window, wprops.x, wprops.y);
+	move_image(self.border, bprops.x, bprops.y);
+	blend_image(self.window, wprops.opa);
+	blend_image(self.border, bprops.opa);
+end
+
+local function keyconf_opacity(self, borderopa, windowopa)
+	blend_image(self.window, windowopa);
+	blend_image(self.border, borderopa);
+end
+
+local function keyconf_tofront(self)
+	self.order = max_current_image_order() + 1;
+	order_image(self.window, self.order+1);
+	order_image(self.border, self.order);
+	if (self.textvid) then
+		order_image(self.textvid, self.order+2);
+	end
+end
+
+local function keyconf_destroy(self)
+	expire_image(self.window, 20);
+	blend_image(self.window, 0.0, 20);
+	expire_image(self.border, 20);
+	blend_image(self.border, 0.0, 20);
+
+	delete_image(self.textvid);
+	self.active = true;
+	self:flush();
+end
 
 local function keyconf_renderline(self, string, size)
 	if (self.textvid) then
@@ -59,21 +107,36 @@ local function keyconf_renderline(self, string, size)
 	self.textvid = render_text([[\ffonts/default.ttf,]] .. size .. " " .. string);
 	self.line = string;
 	prop = image_surface_properties(self.textvid);
-	resize_image(self.bgwindow, prop.width + 20, prop.height + 20, 0);
-	move_image(self.bgwindow, (VRESW - (prop.width + 20)) / 2, (VRESH - (prop.height + 20)) / 2, 0);
-	prop = image_surface_properties(self.bgwindow);
-	move_image(self.textvid, prop.x + 10, prop.y + 10, 0);
-	order_image(self.textvid, 254);
-	order_image(self.bgwindow, 253);
-	show_image(self.textvid);
-	show_image(self.bgwindow);
+
+	link_image(self.textvid, self.window);
+	image_mask_clear(self.textvid, MASK_OPACITY);
+	image_clip_on(self.textvid);
+
+	move_image(self.anchor, VRESW * 0.5 - prop.width * 0.5, VRESH * 0.5 - (prop.height + 10) * 0.5, 0);
+	resize_image(self.window, prop.width + 10, prop.height + 10 , NOW);
+	resize_image(self.border, prop.width + 16, prop.height + 16, NOW);
+
+	move_image(self.textvid, 5, 5, NOW);
+	order_image(self.textvid, self.order + 2);
+	blend_image(self.textvid, 1.0);
 end
 
 
 local function keyconf_new(self)
-	self.bgwindow = fill_surface(32, 32, 0, 0, 254);
 	self.used = {};
 	self.table = {};
+	self.window_color = keyconf_color;
+	self.window_opacity = keyconf_opacity;
+	self.to_front = keyconf_tofront;
+
+	self.anchor = fill_surface(1, 1, 0, 0, 0);
+	self.yofs = 6;
+
+	self:window_color(255, 255, 255, 0, 0, 164);
+	self:window_opacity(0.9, 0.6);
+	self:to_front();
+	resize_image(self.border, VRESW / 3, VRESH * 0.2, 5);
+	resize_image(self.window, VRESW / 3 - 6, VRESH * 0.2 - 6, 5);
 
 	keyconf_renderline(self, [[\#ffffffWelcome to Arcan keyconfig!\r\nPlease press a button for MENU_ESCAPE (required)]], 18);
 	self.label = [[Please press a button for MENU_ESCAPE (required)]];
@@ -90,17 +153,18 @@ local function keyconf_new(self)
 		ofs = ofs + 1;
 	end
 	
-	if (self.n_players > 0) then
-		for i=1,self.n_players do
-			for j=1,#self.player_group do
-				kind = string.sub(self.player_group[j], 1, 1);
-				self.configlist[ofs] = kind .. "PLAYER" .. i .. "_" .. string.sub(self.player_group[j], 2);
-				ofs = ofs + 1;
-			end
-		end
+end
+
+
+local function keyconf_playerline(self)
+	line = [[Configure player input (press SELECT to continue):\n\r\t\t#Players\t#Buttons\n\r]]
+	if (self.active_group == 0) then
+		line = line .. [[\#ffff00]] .. tostring(self.playercount) .. [[\t\t\#ffffff]] .. tostring(self.buttoncount);
+	else
+		line = line .. [[\#ffffff]] .. tostring(self.playercount) .. [[\t\t\#ffff00]] .. tostring(self.buttoncount);
 	end
 
-	show_image(self.bgwindow);
+	keyconf_renderline(self, line);
 end
 
 -- query the user for the next input table,
@@ -138,11 +202,67 @@ local function keyconf_next_key(self)
 
         return false;
     else
-		self:cleanup();
-		self.active = true;
-        return true;
+-- if we've already configured the player inputs, then we're done. Otherwise, remap temporarily for the "# of players" input..
+		if (self.playerconf) then
+			self:destroy();
+			return true;
+		else
+			self.input = self.input_playersel;
+			self.active_group = 0;
+			self.playercount  = 0;
+			self.buttoncount  = 0;
+			keyconf_playerline(self);
+			return false;
+		end
     end
 end
+
+local function keyconf_inp_playersel(self, inputtable)
+	local lbl = self:match(inputtable);
+
+	if (inputtable.active and lbl) then
+		for ind, val in pairs(lbl) do 
+			if (val == "MENU_UP" or val == "MENU_DOWN") then
+				local dir = 1;
+				if (val == "MENU_DOWN") then dir = -1; end
+				if (self.active_group == 0) then
+					self.playercount = (self.playercount + dir) > 0 and (self.playercount + dir) or 0;
+				else
+					self.buttoncount = (self.buttoncount + dir) > 0 and (self.buttoncount + dir) or 0;
+				end
+			
+			elseif (val == "MENU_LEFT" or val == "MENU_RIGHT") then
+				self.active_group = self.active_group == 1 and 0 or 1;
+
+			elseif (val == "MENU_SELECT") then
+				self.input = self.defaultinput;
+				self.playerconf = true;
+				self.ofs = self.ofs - 1;
+				
+				for i=1, self.playercount do
+					table.insert(default_player_group, "rBUTTON" .. tostring(i));
+				end
+				
+				if (self.playercount > 0 and self.buttoncount > 0) then
+					for i=1,self.playercount do
+						for j=1,#self.player_group do
+							kind = string.sub(self.player_group[j], 1, 1);
+							table.insert(self.configlist, kind .. "PLAYER" .. i .. "_" .. string.sub(self.player_group[j], 2));
+						end
+					end
+				end
+
+				return keyconf_next_key(self);
+			end
+
+			keyconf_playerline(self);
+			return false;
+		end
+	end
+
+	return false;
+end
+
 
 -- return the symstr that match inputtable, or nil.
 local function keyconf_match(self, input, label)
@@ -318,15 +438,6 @@ local function keyconf_running(self)
 	end
 end
 
-local function keyconf_cleanup(self)
-	if (self) then
-		delete_image(self.textvid);
-		delete_image(self.bgwindow);
-		self.active = true;
-		self:flush();
-	end
-end
-
 -- set the current working table.
 -- for each stored entry, set prefix if defined 
 function keyconf_create(nplayers, menugroup, playergroup, keyname)
@@ -336,16 +447,17 @@ function keyconf_create(nplayers, menugroup, playergroup, keyname)
 		renderline = keyconf_renderline,
 		next_key = keyconf_next_key,
 		analoginput = keyconf_analog,
+		defaultinput = keyconf_input,
 		flush = keyconf_flush,
 		id = keyconf_tbltoid,
 		input = keyconf_input,
-		cleanup = keyconf_cleanup,
+		destroy = keyconf_destroy,
 		set = keyconf_set,
---
 		labels = keyconf_labels,
 		ignore_modifiers = false,
 		n_players = nplayers,
-		keyfile = keyname
+		keyfile = keyname,
+		input_playersel = keyconf_inp_playersel	
 	};
 
 	local players = 0;
@@ -355,17 +467,7 @@ function keyconf_create(nplayers, menugroup, playergroup, keyname)
 
 -- command-line overrides
 	for k,v in ipairs(arguments) do
-		if (string.sub(v, 1, 8) == "players=") then
-			local plc = tonumber( string.sub(v, 9) );
-			if (plc and plc > 0) then restbl.n_players = plc; end
-		elseif (string.sub(v, 1, 8) == "buttons=") then
-			local bc = tonumber( string.sub(v, 9) );
-			if (bc and bc > 0) then
-				for i=1,bc do
-					table.insert(default_player_group, "rBUTTON" .. tostring(i));
-				end
-			end
-		elseif (string.sub(v, 1, 8) == "keyname=") then
+		if (string.sub(v, 1, 8) == "keyname=") then
 			local fn = string.sub(v, 9);
 			if (string.len(fn) > 0) then
 				restbl.keyfile = fn;
