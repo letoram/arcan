605a2f8597db72024972fa52872bde83179b806b
bjornstahl@34ee8734-9d04-4b2f-9570-a8dab4294b71 <bjornstahl@34ee8734-9d04-4b2f-9570-a8dab4294b71>
2012-07-18 22:50:39 +0000

    OSX and their shoddy POSIX conformance again, added workaround when clock_gettime is unavailable. Minor buildfixes so OSX support is sortof back on track again.
    FossilOrigin-Name: f27baae77ec9497eb3601869bfb3e2c3cb7cc86d4d38032da71be1fad273b6c2

--- arcan_frameserver_backend.h
+++ arcan_frameserver_backend.h
@@ -68,8 +68,6 @@ typedef struct {
 	bool ready;
 } arcan_frameserver_meta;
 
-typedef struct SDL_mutex SDL_mutex;
-
 typedef struct {
 /* video / audio properties used */
 	arcan_frameserver_meta desc;
