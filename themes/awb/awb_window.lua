da5bc35c681c7f67a99a3753af61f8dc26ec6df0
bjornstahl@34ee8734-9d04-4b2f-9570-a8dab4294b71 <bjornstahl@34ee8734-9d04-4b2f-9570-a8dab4294b71>
2013-06-08 11:56:47 +0000

    (awb) backup, still not useful but getting there.
    FossilOrigin-Name: b753c36907f6a7e2020526c5992470c47739381d8d7f0e3b330eebae6cbbf69c

--- themes/awb/awb_window.lua
+++ themes/awb/awb_window.lua
@@ -23,13 +23,14 @@ local function awbwnd_show(self)
 end
 
 local function awbwnd_reorder(self, orderv)
-	order_image(self.bordert, orderv+1);
-	order_image(self.borderd, orderv+1);
-	order_image(self.borderl, orderv+1);
-	order_image(self.borderr, orderv+1);
+	order_image(self.bordert, orderv);
+	order_image(self.borderd, orderv);
+	order_image(self.borderl, orderv);
+	order_image(self.borderr, orderv);
 	order_image(self.canvas, orderv);
+
 	for key, val in pairs(self.directions) do
-			val:reorder(orderv+1);
+			val:reorder(orderv);
 	end
 end
 
@@ -85,7 +86,10 @@ local function awbwnd_alloc(self)
 -- a placeholder, the :container(vid) method will do the rest 
 --
 	if (self.mode == "container") then
-		self.canvas = fill_surface(1, 1, 0, 0, 0);
+		self.canvas = fill_surface(cwidth, cheight, 0, 0, 0); 
+		move_image(self.canvas, cx, cy);
+		link_image(self.canvas, self.anchor);
+		show_image(self.canvas);	
 
 	elseif (self.mode == "container_managed") then
 		self.canvas = fill_surface(1, 1, 0, 0, 0);
@@ -181,18 +185,22 @@ local function awbwnd_resize(self, neww, newh)
 -- sure that we accompany minimum thickness and height
 	if (self.directions["left"] ~= nil) then 
 		minw_pad = minw_pad + self.directions["left"].thickness;
+		minh_pad = minh_pad + self.directions["left"].minheight;
 	end
 
 	if (self.directions["right"] ~= nil) then
 		minw_pad = minw_pad + self.directions["right"].thickness;
+		minh_pad = minh_pad + self.directions["right"].minheight;
 	end
 
 	if (self.directions["top"] ~= nil) then
 		minh_pad = minh_pad + self.directions["top"].thickness;
+		minw_pad = minw_pad + self.directions["top"].minheight;
 	end
 
-	if (self.directions["top"] ~= nil) then
-		minh_pad = minh_pad + self.directions["top"].thickness;
+	if (self.directions["bottom"] ~= nil) then
+		minh_pad = minh_pad + self.directions["bottom"].thickness;
+		minw_pad = minw_pad + self.directions["bottom"].minwidth;
 	end
 
 	if (self.minw < minw_pad * 2) then
@@ -231,6 +239,8 @@ local function awbwnd_addbar(self, direction, active_resdescr, inactive_resdescr
 	newdir.activeres = active_resdescr;
 	newdir.inactvres = inactive_resdescr;
 	newdir.thickness = thickness;
+	newdir.minwidth  = 0;
+	newdir.minheight = 0;
 	newdir.refresh   = awbwbar_refresh;
 	newdir.vertical  = direction == "left" or direction == "right";
 	newdir.direction = direction;
@@ -280,6 +290,74 @@ local function awbwnd_own(self, vid)
 	end
 end
 
+local function awbwnd_refreshicons(self)
+--
+-- based on orientation, "flood fill" with icons
+-- until we run out of space
+-- 
+	local max_y  = self.height;
+	local max_x  = self.width;
+	local step_row;
+	local step_col;
+	local start_x = 0;
+	local start_y = 0;
+	
+	if (self.iconalign == "top") then
+		step_col = {self.spacing, 0};
+		step_row = {0, self.spacing};
+
+	elseif (self.iconalign == "bottom") then
+		step_col = {0, self.spacing};
+		step_row = {-self.spacing, 0};
+		start_y  = max_y;
+
+	elseif (self.iconalign == "left") then
+		step_col = {0, self.spacing};
+		step_row = {self.spacing, 0};
+
+	elseif (self.iconalign == "right") then
+		step_col = {0, self.spacing};
+		step_row = {-self.spacing, 0};
+		start_x  = max_x; 
+
+	else
+		warning("awbwnd_refreshicons() -- poor icon alignment specified.");
+	end
+
+	for ind, val in pairs(self.icons) do
+		local aprops = image_surface_properties(val.active);
+		local lprops = image_surface_properties(val.label);
+		local mwidth = aprops.width > lprops.width 
+			and aprops.width or lprops.width;
+		local mheight= aprops.height > lprops.height
+			and aprops.height or lprops.height;
+
+-- left and right need to be shifted based on their height	
+		show_image({val.active, val.label});
+		move_image(val.active, curx, cury);
+	end
+end
+
+local function awbwnd_addicon(self, name, img, imga, font, fontsz, trigger)
+	if (self.icons[name] ~= nil) then
+		delete_image(self.icons[name].main);
+		delete_image(self.icons[name].mainsel);
+	end
+
+	local newent   = {};
+	newent.main    = type(img) == "string" and load_image(img) or img;
+	newent.mainsel = type(img) == "string" and load_image(imga) or img;
+	newent.active  = newent.main;
+	newent.label   = render_text(string.format("\\f%s,%d %s", font, 
+		fontsz, name)); 
+	newent.trigger = trigger;
+	
+	self.icons[name] = newent;
+	local props = image_surface_properties(newent.main);	
+
+	self:refresh_icons();
+end
+
 local function awbwnd_canvas(self, newvid)
 	if (self.mode == "container") then
 		a = true;
@@ -310,6 +388,7 @@ function awbwnd_create(options)
 		borderw = 2,
 		minw = 0,
 		minh = 0,
+		spacing = 16,
 		activestate = true,
 
 -- dynamic default properties
@@ -323,6 +402,12 @@ function awbwnd_create(options)
 		for key, val in pairs(options) do
 			restbl[key] = val;
 		end
+	end
+
+	if (restbl.mode == "iconview") then
+		restbl.add_icon = awbwnd_addicon;
+		restbl.refresh_icons = awbwnd_refreshicons;
+		restbl.icons = {};
 	end
 
 	if (restbl.fullscreen) then
