c3a38c1db339efbc4a9694ef746fcaca2c0eda96
bjornstahl <bjornstahl@34ee8734-9d04-4b2f-9570-a8dab4294b71>
2013-07-26 12:14:37 +0000

    (awb) listview scrollbar click, drag / drop       added dialog on quit

--- themes/awb/awbwnd_list.lua
+++ themes/awb/awbwnd_list.lua
@@ -1,22 +1,47 @@
 --
 -- AWB List View
 -- Specialized window subclass to handle longer multi-column lists
+-- With clickable headers, sorting etc.
 -- 
--- Todo;
--- Column headers and resizers between each row (alternative column bg)
--- Deregister mouse handler on exit
--- Edit box filtering
---
+local function awblist_scrollbar(self)
+	if (self.capacity > self.total) then
+		if (self.scroll) then
+			self.scroll = false;
+			hide_image(self.dir[self.icon_bardir].fill.vid);
+			self.dir[self.icon_bardir].oldsz = self.dir[self.icon_bardir].bsize;
+			self.dir[self.icon_bardir].bsize = 0;
+			self:list_resize(self.w, self.h);
+		end
+
+		return;
+
+	elseif (self.capacity < self.total and self.scroll == false) then
+		self.scroll = true;
+		show_image(self.dir[self.icon_bardir].fill.vid);
+		self.dir[self.icon_bardir].bsize = self.dir[self.icon_bardir].oldsz;
+		self:list_resize(self.w, self.h);
+	end
+
+	local prop = image_surface_properties(self.dir[self.icon_bardir].fill.vid);
+	local stepsz = prop.height / self.total;
+	
+	resize_image(self.scrollcaret, self.dir[self.icon_bardir].size - 2,
+		stepsz * self.capacity);
+	move_image(self.scrollcaret, 1, stepsz * (self.ofs - 1)); 
+end
 
 function awblist_resize(self, neww, newh)
 	self:list_resize(neww, newh);
 	local props = image_surface_properties(self.canvas.vid);
+	self.capacity = math.ceil(props.height / (self.lineh + self.linespace)) + 1;
 
--- only redraw if we've grown (keep image when shrinking)
-	if (props.height - self.lasth > self.lineh) then
+-- only redraw if we've grown (keep image when shrinking), some parts of
+-- this function is really expensive and high-resolution mice etc. do
+-- emitt lots of resize events when drag-resizing
+	if (props.height - self.lasth > (self.lineh + self.linespace)) then
 		self.lasth  = props.height;
-		self.restbl = self:datasel(self.ofs, math.ceil(newh / self.lineh));
-
+		self.restbl, self.total = self:datasel(self.ofs, self.capacity);	
+		
 		for i, v in ipairs(self.listtemp) do
 			delete_image(v);
 		end
@@ -25,8 +50,7 @@ function awblist_resize(self, neww, newh)
 -- Render each column separately, using a clipping anchor 
 		local xofs = 0;
 		for ind, col in ipairs(self.cols) do
-			local clip = null_surface(math.floor(
-				props.width * col), props.height);
+			local clip = null_surface(math.floor(props.width * col), props.height);
 	
 			link_image(clip, self.canvas.vid);
 			show_image(clip);
@@ -35,22 +59,52 @@ function awblist_resize(self, neww, newh)
 			image_tracetag(clip, "listview.col(" .. tostring(ind) .. ").clip");
 			xofs = xofs + math.floor(props.width * col);
 
+-- concat the subselected column lines, force-add headers to the top
 			local rendtbl = {};
 			for i, v in ipairs(self.restbl) do
 				table.insert(rendtbl, v.cols[ind]);
 			end
-
 			local colv, lines = self.renderfn(table.concat(rendtbl, [[\n\r]]));
+	
 			self.line_heights = lines;
 
 			link_image(colv, clip);
 			show_image(colv);
+			move_image(colv, 0, self.linespace * 0.5);
 			image_tracetag(colv, "listview.col(" .. tostring(ind) ..").column");
 			image_inherit_order(colv, true);
+			order_image(colv, 2);
 			image_mask_set(colv, MASK_UNPICKABLE);
 			image_clip_on(colv, CLIP_SHALLOW);
-			table.insert(self.listtemp, clip);
+			table.insert(self.listtemp, clip);  
 		end
+
+-- hilighted striped bars to make it easier to distinguish between items
+		if (self.bglines) then
+			for i,v in ipairs(self.bglines) do
+				delete_image(v);
+			end
+			
+			self.bglines = {};
+
+			local yofs = 0;
+			for i=2,#self.line_heights do
+				if (i % 2 == 0) then
+					a = color_surface(props.width, 
+						self.line_heights[i] - self.line_heights[i - 1], 
+						self.rowhicol[1], self.rowhicol[2], self.rowhicol[3]);
+					move_image(a, 0, self.line_heights[i]);
+					link_image(a, self.canvas.vid);
+					show_image(a);
+					image_inherit_order(a, true);
+					image_clip_on(a);
+					image_tracetag(a, "listview.stripbg");
+					image_mask_set(a, MASK_UNPICKABLE);
+					table.insert(self.bglines, a);
+				end
+			end
+		end
+
 	end
 
 -- always update clipping anchors
@@ -62,7 +116,14 @@ function awblist_resize(self, neww, newh)
 		xofs = xofs + clipw;
 	end
 
-	resize_image(self.cursor, props.width, self.lineh);
+	if (self.bglines) then
+		for i,v in ipairs(self.bglines) do
+			resize_image(v, props.width, image_surface_properties(v).height);
+		end
+	end
+
+	resize_image(self.cursor, props.width, self.lineh + self.linespace);
+	awblist_scrollbar(self);
 end
 
 function awbicon_liney(self, yv)
@@ -84,33 +145,99 @@ function awbicon_liney(self, yv)
 	return 0, nil;
 end
 
-function awbwnd_listview(pwin, lineh, colcfg, datasel_fun, render_fun,
-	scrollbar_icn, scrollcaret_icn, cursor_icn, bardir, options)
+local function clampofs(self)
+	if (self.ofs < 1) then
+		self.ofs = 1;
+	elseif (self.ofs + self.capacity >= self.total) then
+		self.ofs = self.total - self.capacity;
+	end
+end
+
+local function scrollup(self, n)
+	self.ofs = self.ofs - math.abs(n);
+	clampofs(self);
+	self.lasth = 0;
+	self:resize(self.w, self.h);
+end
+
+local function scrolldown(self, n)
+	self.ofs = self.ofs + math.abs(n);
+	clampofs(self);
+	self.lasth = 0;
+	self:resize(self.w, self.h);
+end
+
+local function caretdrop(self)
+	self.caret_dy = 0;
+end
+
+local function caretdrag(self, vid, dx, dy)
+	local prop = image_surface_properties(
+		self.wnd.dir[self.wnd.icon_bardir].fill.vid);
+
+	local stepsz = prop.height / self.wnd.total;
+	self.caret_dy = self.caret_dy + dy;
+
+	if(self.caret_dy < -5) then
+		local steps = math.floor(-1 * self.caret_dy / 5);
+		self.caret_dy = self.caret_dy + steps * 5;
+		scrollup(self.wnd, steps); 
+
+	elseif (self.caret_dy > 5) then
+		local steps = math.floor(self.caret_dy / 5);
+		self.caret_dy = self.caret_dy - steps * 5;
+		scrolldown(self.wnd, steps);
+	end
+end
+
+local function scrollclick(self, vid, x, y)
+	local props = image_surface_resolve_properties(self.caret);
+
+	self.wnd.ofs = (y < props.y) and (self.wnd.ofs - self.wnd.capacity) or
+		(self.wnd.ofs + self.wnd.capacity);
+
+	clampofs(self.wnd);
+
+	self.wnd.lasth = 0;
+	self.wnd:resize(self.wnd.w, self.wnd.h);
+end
+
+function awbwnd_listview(pwin, lineh, linespace, colcfg, datasel_fun, 
+	render_fun,scrollbar_icn, scrollcaret_icn, cursor_icn, bardir,
+	options)
 
 	if (bardir == nil) then
 		bardir = "r";
 	end
 
-	pwin.cell_w  = cell_w;
-	pwin.cell_h  = cell_h;
-	
+-- overridable options
+	pwin.cell_w   = cell_w;
+	pwin.cell_h   = cell_h;
+	pwin.rowhicol = {0, 0, 0};
+	pwin.bglines  = {};
+
+-- apply user options
 	if (options) then
 		for k,v in pairs(options) do
 			pwin[k] = v;
 		end
 	end
 
-	pwin.lasth     = 0;
-	pwin.lineh     = lineh;
-	pwin.ofs       = 1;
-	pwin.listtemp  = {};
-	pwin.datasel   = datasel_fun;
-	pwin.renderfn  = render_fun;
-	pwin.cols      = colcfg; 
+-- protect against namespace collisions
+	pwin.lasth      = 0;
+	pwin.lineh      = lineh;
+	pwin.linespace  = linespace;
+	pwin.ofs        = 1;
+	pwin.listtemp   = {};
+	pwin.datasel    = datasel_fun;
+	pwin.renderfn   = render_fun;
+	pwin.cols       = colcfg;
+	pwin.scrollup   = stepup;
+	pwin.scrolldown = stepdown;
 
 	pwin.icon_bardir = bardir;
 	pwin.scrollcaret = scrollcaret_icn;
---	pwin.scrollbar   = awbicon_setscrollbar;
+	pwin.scroll      = true;
 	pwin.line_y      = awbicon_liney;
 
 	image_tracetag(scrollbar_icn,   "awbwnd_listview.scrollbar");
@@ -124,20 +251,41 @@ function awbwnd_listview(pwin, lineh, colcfg, datasel_fun, render_fun,
 
 	link_image(scrollcaret_icn, newicn.vid);
 	image_inherit_order(scrollcaret_icn, true);
-	order_image(scrollcaret_icn, 1);
+	order_image(newicn.vid, 3);
+	order_image(scrollcaret_icn, 3);
 	show_image(scrollcaret_icn);
-	resize_image(scrollcaret_icn, 
+	resize_image(scrollcaret_icn,
 		pwin.dir["t"].size - 2, pwin.dir["t"].size - 2); 
+	image_clip_on(scrollcaret_icn, CLIP_SHALLOW);
 
 	pwin.list_resize = pwin.resize;
 	pwin.resize = awblist_resize;
 
 	link_image(cursor_icn, pwin.canvas.vid);
 	image_inherit_order(cursor_icn, true);
+	order_image(cursor_icn, 1);
 	blend_image(cursor_icn, 0.8);
 	pwin.cursor = cursor_icn;
 	image_clip_on(cursor_icn, CLIP_SHALLOW);
 	image_mask_set(cursor_icn, MASK_UNPICKABLE);
+
+	local carretmh = {
+		wnd  = pwin,
+		caret_dy = 0,
+		drag = caretdrag,
+		drop = caretdrop,
+		own  = function(self,vid) return scrollcaret_icn == vid; end
+	};
+
+	local scrollmh = {
+		caret = pwin.scrollcaret,
+		wnd   = pwin, 
+		own   = function(self, vid) return newicn.vid == vid; end,
+		click = scrollclick; 
+	};
+
+	mouse_addlistener(scrollmh, {"click"});
+	mouse_addlistener(carretmh, {"drag", "drop"});
 
 -- find cursor ..
 	local mhand = {};
@@ -160,7 +308,13 @@ function awbwnd_listview(pwin, lineh, colcfg, datasel_fun, render_fun,
 		return pwin.canvas.vid == vid;
 	end
 
-	mouse_addlistener(mhand, {"dblclick", "motion"}); 
+	mouse_addlistener(mhand, {"dblclick", "motion"});
+
+	pwin.on_destroy = function()
+		mouse_droplistener(scrollmh);
+		mouse_droplistener(carretmh);
+		mouse_droplistener(mhand);
+	end
 --
 -- selected cursor management linked to canvas
 -- 
