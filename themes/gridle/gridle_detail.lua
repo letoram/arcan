da063bbad0b7b34535f07e56753c9ee8d583f896
arcan-fe@nospam <arcan-fe@nospam>
2014-01-25 18:46:55 +0000

    (gridle) detailview fixes
    FossilOrigin-Name: 011fb761ff54ae9cc1a273f4afa58bfb4e2753ad621ea3d08d53475c77a7264e

--- themes/gridle/gridle_detail.lua
+++ themes/gridle/gridle_detail.lua
@@ -5,15 +5,26 @@ detailview = {
 
 local function gridledetail_load()
 -- non-visible 3d object as camtag
-	detailview.dir_light = load_shader("shaders/dir_light.vShader", "shaders/dir_light.fShader", "default3d");
-	shader_uniform(detailview.dir_light, "map_diffuse", "i", PERSIST, 0);
-	shader_uniform(detailview.dir_light, "wlightdir", "fff", PERSIST, 1.0, 0.0, 0.0);
-	shader_uniform(detailview.dir_light, "wambient",  "fff", PERSIST, 0.3, 0.3, 0.3);
-	shader_uniform(detailview.dir_light, "wdiffuse",  "fff", PERSIST, 0.3, 0.3, 0.3);
+	detailview.dir_light = load_shader("shaders/dir_light.vShader", 
+		"shaders/dir_light.fShader", "default3d");
+
+	shader_uniform(detailview.dir_light, 
+		"map_diffuse", "i", PERSIST, 0);
+
+	shader_uniform(detailview.dir_light, "wlightdir", "fff", 
+		PERSIST, 1.0, 0.0, 0.0);
+
+	shader_uniform(detailview.dir_light, "wambient", 
+		"fff", PERSIST, 0.3, 0.3, 0.3);
+
+	shader_uniform(detailview.dir_light, "wdiffuse", 
+		"fff", PERSIST, 0.3, 0.3, 0.3);
+
 	detailview.loaded = true;
 end
 
--- figure out what to show based on a "source data string" (detailres, dependency to havedetails) and a gametable
+-- figure out what to show based on a "source data string" 
+-- (detailres, dependency to havedetails) and a gametable
 local function gridledetail_buildview(detailres, gametbl )
 	video_3dorder(ORDER_LAST);
 
@@ -24,11 +35,13 @@ local function gridledetail_buildview(detailres, gametbl )
 	if (detailview.model) then
 		image_shader(detailview.model.vid, detailview.dir_light);
 		
--- we can hardcode these values because the "scale vertices" part forces the actual value range of any model hierarchy to -1..1
+-- we can hardcode these values because the "scale vertices" 
+-- part forces the actual value range of any model hierarchy to -1..1
 		detailview.startpos = {x = -1.0, y = 0.0, z = -4.0};
 		detailview.startang = {roll = 0, pitch = 0, yaw = 0};
 			
--- if the model specifies a default view pos (position + roll/pitch/yaw), set that one as the target)
+-- if the model specifies a default view pos (position + 
+-- roll/pitch/yaw), set that one as the target)
 		detailview.zoompos   = detailview.model.screenview.position;
 		detailview.zoomang   = detailview.model.screenview.orientation;
 		detailview.zoompos.x = detailview.zoompos.x * -1;
@@ -53,8 +66,11 @@ local function gridledetail_freeview(axis, mag)
 		elseif (axis == 2) then dz = mag;
 	end
 
-	move3d_model(detailview.model.vid, detailview.startpos.x + dx,
-		detailview.startpos.y + dy, detailview.startpos.z + dz, 20 + settings.transitiondelay);
+	move3d_model(detailview.model.vid, 
+		detailview.startpos.x + dx,
+		detailview.startpos.y + dy, 
+		detailview.startpos.z + dz, 20 + settings.transitiondelay
+	);
 
 	detailview.model:destroy(settings.transitiondelay);
 	
@@ -64,15 +80,28 @@ end
 
 function gridledetail_internal_status(source, datatbl)
 	if (datatbl.kind == "resized") then
-		detailview.model:update_display(source);
+		local flipped = datatbl.mirrored ~= 1;
+		local displayimg = null_surface(1,1);
+		image_sharestorage(source, displayimg);
+		detailview.model:update_display(displayimg, flipped); 
 	
 		audio_gain(datatbl.source_audio, settings.internal_gain, NOW);
-		move3d_model(detailview.model.vid, detailview.zoompos.x, detailview.zoompos.y, detailview.zoompos.z, 20);
-		rotate3d_model(detailview.model.vid, detailview.zoomang.roll, detailview.zoomang.pitch, detailview.zoomang.yaw, 20, ROTATE_ABSOLUTE);
+		move3d_model(detailview.model.vid, 
+			detailview.zoompos.x, 
+			detailview.zoompos.y, 
+			detailview.zoompos.z, 20
+		);
+
+		rotate3d_model(detailview.model.vid, 
+			detailview.zoomang.roll, 
+			detailview.zoomang.pitch, 
+			detailview.zoomang.yaw, 20, ROTATE_ABSOLUTE
+		);
 	end
 end
 
--- just use this to make sure we don't load models before the animation has finished.  
+-- just use this to make sure we don't load 
+-- models before the animation has finished.  
 function gridledetail_clock_pulse(tick)
 	timestamp = tick;
 	if (detailview.cooldown > 0) then 
@@ -80,10 +109,18 @@ function gridledetail_clock_pulse(tick)
 	end
 end
 
--- switch between running with fullscreen and running with cabinet zoomed in
+local orig_order = order_image;
+function trace_order(vid, order)
+	orig_order(vid, order);
+end
+
+-- switch between running with fullscreen and 
+-- running with cabinet zoomed in
 local function gridledetail_switchfs()
+
 	if (detailview.fullscreen) then
--- undo vectordisplay etc. remove the "black image" used to block background
+-- undo vectordisplay etc. remove the 
+-- "black image" used to block background
 		gridlemenu_rebuilddisplay();
 		hide_image(internal_vid);
 		delete_image(internal_vidborder); 
@@ -99,20 +136,29 @@ local function gridledetail_switchfs()
 	else
 		hide_image(detailview.model.vid);
 
--- setup black frame around output, then initialze effects etc.
+-- setup black frame around output, 
+-- then initialze effects etc.
 		internal_vidborder = instance_image( imagery.black );
+		image_tracetag(internal_vidborder, "detailfs_background");
 		detailview.fullscreen = true;
 		image_mask_clearall(internal_vidborder);
 		order_image(internal_vidborder, INGAMELAYER_BACKGROUND);
 		resize_image(internal_vidborder, VRESW, VRESH);
 		show_image(internal_vidborder);
 
+		image_tracetag(internal_vid,"internal_vid(detailfs)");
+		show_image(internal_vid);
+		image_mask_clear(internal_vid, MASK_OPACITY);
+
+		order_image = trace_order;
+
 		gridlemenu_rebuilddisplay(settings.internal_toggles);
 	end
 end
 
 function gridledetail_stopinternal()
--- stop the internal launch, zoom out the model and replace display with static
+-- stop the internal launch, zoom out 
+-- the model and replace display with static
 	if (valid_vid( internal_vidborder )) then 
 		delete_image(internal_vidborder);
 		internal_vidborder = nil;
@@ -140,11 +186,17 @@ function gridledetail_stopinternal()
 	internal_vid = BADID;
 
 -- reset model to start position
-	rotate3d_model(detailview.model.vid, 0, 0, 0, 20, ROTATE_ABSOLUTE);
-	move3d_model(detailview.model.vid, detailview.startpos.x, detailview.startpos.y, detailview.startpos.z, 20);
+	rotate3d_model(detailview.model.vid, 
+		0, 0, 0, 20, ROTATE_ABSOLUTE);
+	move3d_model(detailview.model.vid, 
+		detailview.startpos.x, 
+		detailview.startpos.y, 
+		detailview.startpos.z, 20
+	);
 end
 
--- regular dispatch-input, but we override MENU/etc. to support switching between fullscreen and not
+-- regular dispatch-input, but we override MENU/etc. 
+-- to support switching between fullscreen and not
 function gridledetail_internalinput(iotbl)
 	local restbl = keyconfig:match(iotbl);
 	local addlbl = "";
@@ -158,7 +210,8 @@ function gridledetail_internalinput(iotbl)
 				if (val == "MENU_TOGGLE" and detailview.fullscreen) then
 					gridlemenu_internal(internal_vid, true, true);
 	
--- iotbl.active filter here is just to make sure we don't save twice (press and release) 
+-- iotbl.active filter here is just to make sure 
+-- we don't save twice (press and release) 
 				elseif (val == "QUICKSAVE" or val == "QUICKLOAD") then
 					internal_statectl("quicksave", val == "QUICKSAVE");
 
@@ -195,14 +248,28 @@ local function gridledetail_contextinput()
 	if (detailview.zoomed) then
 		detailview.zoomed = false;
 		if (detailview.modeldisplay_aid ~= BADID) then
-			audio_gain(detailview.modeldisplay_aid, settings.movieagain * 0.5, 20);
+			audio_gain(detailview.modeldisplay_aid, 
+				settings.movieagain * 0.5, 20);
 		end
 
-		move3d_model(detailview.model.vid, detailview.startpos.x, detailview.startpos.y, detailview.startpos.z, 20);
+		move3d_model(detailview.model.vid, 
+			detailview.startpos.x, 
+			detailview.startpos.y, 
+			detailview.startpos.z, 20
+		);
 		rotate3d_model(detailview.model.vid, 0, 0, 0, 20, ROTATE_ABSOLUTE);
 	else
-		move3d_model(detailview.model.vid, detailview.zoompos.x, detailview.zoompos.y, detailview.zoompos.z, 20);
-		rotate3d_model(detailview.model.vid, detailview.zoomang.roll, detailview.zoomang.pitch, detailview.zoomang.yaw, 20, ROTATE_ABSOLUTE);
+		move3d_model(detailview.model.vid, 
+			detailview.zoompos.x, 
+			detailview.zoompos.y, 
+			detailview.zoompos.z, 20);
+
+		rotate3d_model(detailview.model.vid, 
+			detailview.zoomang.roll, 
+			detailview.zoomang.pitch,
+			detailview.zoomang.yaw, 
+			20, ROTATE_ABSOLUTE);
+
 		if (detailview.modeldisplay_aid ~= BADID) then
 			audio_gain(detailview.modeldisplay_aid, settings.movieagain, 20);
 		end
@@ -245,7 +312,8 @@ local function find_detail(step, zout)
 		elseif (nextind > #settings.games) then nextind = 1; end
 
 		if (settings.games[nextind].resources == nil) then
-			settings.games[nextind].resources = resourcefinder_search( settings.games[nextind], true); 
+			settings.games[nextind].resources = 
+				resourcefinder_search( settings.games[nextind], true); 
 		end
 
 		detailres = find_cabinet_model(settings.games[nextind]);
@@ -259,10 +327,13 @@ local function find_detail(step, zout)
 		gridledetail_freeview(2, 6.0);
 		gridledetail_buildview(detailres, settings.games[ nextind ])
 
--- start "far away" and quickly zoom in, while that happens, prevent some keys from being used ("cooldown")
+-- start "far away" and quickly zoom in, while 
+-- that happens, prevent some keys from being used ("cooldown")
 		show_image(detailview.model.vid);
 		move3d_model(detailview.model.vid, -1.0, 0.0, zout);
-		move3d_model(detailview.model.vid, -1.0, 0.0, -4.0, settings.transitiondelay);
+		move3d_model(detailview.model.vid, 
+			-1.0, 0.0, -4.0, settings.transitiondelay);
+
 		detailview.zoomed = false;
 		detailview.zoompress = nil;
 		detailview.cooldown = settings.transitiondelay;
@@ -333,13 +404,18 @@ function gridledetail_show(detailres, gametbl, ind)
 -- launch based on current preferences and target capabilities 
 	detailview.iodispatch["MENU_SELECT"] = function(iotbl)
 		local captbl = launch_target_capabilities( detailview.game.target )
-		local launch_internal = (settings.default_launchmode == "Internal" or captbl.external_launch == false) and captbl.internal_launch;
+		local launch_internal = (settings.default_launchmode 
+			== "Internal" or captbl.external_launch == false) 
+			and captbl.internal_launch;
 		
 			if (launch_internal) then
 			settings.capabilities = captbl;
 				gridle_load_internal_extras();
 
-				internal_vid = launch_target( detailview.game.gameid, LAUNCH_INTERNAL, gridledetail_internal_status );
+				internal_vid = launch_target(
+					detailview.game.gameid, LAUNCH_INTERNAL, 
+					gridledetail_internal_status );
+	
 				if (internal_vid) then
 					settings.internal_txcos = image_get_txcos(internal_vid);
 
