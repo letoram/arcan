d4736694a034674893c1703648ddb67a169fe4c3
arcan-fe@nospam <arcan-fe@nospam>
2014-02-14 07:56:48 +0000

    (doc) fleshed out pick_items
    FossilOrigin-Name: c08b2c8dfbf369c3746260b1ed2b823f979320d4012b3111092f19258fa3d1a4

--- doc/pick_items.lua
+++ doc/pick_items.lua
@@ -2,9 +2,20 @@
 -- @short: return a list of *limit* objects whose bounding 
 -- region overlap x,y in screen space.
 -- @inargs: x, y, limit, *reverse*   
--- @outargs: 
--- @longdescr:
--- @note: the results of this function is undefined if an object
+-- @outargs: res_table
+-- @longdescr: pick_items can be used as a crude interface to 
+-- implement collision detection and response. The default behavior
+-- is to sweep the pipeline based on the order value from lowest to 
+-- highest, stopping after *limit* objects have been found. If 
+-- *reverse* is set to a !0 value, the scan will instead start from
+-- the highest to the lowest order. The return value will be an 
+-- indexed table of 0..limit VIDs.
+--
+-- @note: the cost for calling pick_items vary with the complexity
+-- of the object in the scene, and increase noticeably with the 
+-- progression (2D- object) -> (x,y rotated 2D object) -> (x,y,z
+-- rotated 2D object / 3D object). 
+-- @note: the behavior of pick_items is undefined if an object
 -- uses a non-rectangular representation (e.g. custom defined 2D 
 -- surface or transformed with a vertex shader.
 -- @group: image 
