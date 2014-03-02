a2fe9994443a7a1fa032c7af955f7e8a7de4e5e2
bjornstahl <arcan-fe@nospam>
2014-03-02 22:27:23 +0100

    (core+) a lot of the problems with the framequeue seems to have been darwin being terrible (no unnamed semaphores silent fail)

---
+++ platform/darwin/sem.c
@@ -0,0 +1,74 @@
+/* Arcan-fe (OS/device platform), scriptable front-end engine
+ *
+ * Arcan-fe is the legal property of its developers, please refer
+ * to the platform/LICENSE file distributed with this source distribution
+ * for licensing terms.
+ */
+
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <errno.h>
+#include <time.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <arcan_math.h>
+#include <arcan_general.h>
+
+int arcan_sem_post(sem_handle sem)
+{
+	return sem_post(sem);
+}
+
+int arcan_sem_unlink(sem_handle sem, char* key)
+{
+	return sem_unlink(key);
+}
+
+int arcan_sem_trywait(sem_handle sem)
+{
+	return sem_trywait(sem);
+}
+
+int arcan_sem_wait(sem_handle sem)
+{
+	return sem_wait(sem);
+}
+
+/* 
+ * In its infinite wisdom, darwin only supports 
+ * named semaphores (seriously). We are forced to
+ * either rewrite some parts for this, or simulate
+ * named through unnamed.
+ */ 
+int arcan_sem_init(sem_handle* sem, unsigned val)
+{
+	char tmpbuf[32];
+	int retryc = 10;
+
+
+	while(retryc--){
+		snprintf(tmpbuf, 32, "/arc_dwn_sem_%d", rand());
+		*sem = sem_open(tmpbuf, O_CREAT | O_EXCL, 0700, val);
+
+/* note: sem_unlink is distinct from sem_close ;-) */
+		sem_unlink(tmpbuf);
+
+		if (SEM_FAILED != *sem){
+			return 0;
+		}
+	}
+
+	return -1;
+}
+
+int arcan_sem_destroy(sem_handle sem)
+{
+	if (!sem)
+		return -1;
+	
+	int rv = sem_close(sem);
+	return rv;
+}
