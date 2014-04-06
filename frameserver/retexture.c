8163d84ef0db903bd99c0be6610bd0e31f628ab5
arcan-fe@nospam <arcan-fe@nospam>
2014-04-06 17:32:33 +0000

    (frameserver) work on in-situ texture replacement for 3d cores and hijacked targets.
    FossilOrigin-Name: 8a41d9b7ba5f167408a101f6fc6641688e834dae23ae18a347d2dd64a50993e1

--- frameserver/retexture.c
+++ frameserver/retexture.c
@@ -26,8 +26,19 @@
 #include <unistd.h>
 #include <string.h>
 #include <arcan_shmif.h>
+#include <assert.h>
+
+#include "retexture.h"
 
 #include GL_HEADERS
+
+#ifndef RETEXTURE_PREFIX 
+#define RETEXTURE_PREFIX 
+#endif
+
+#define MERGE(X,Y) X ## Y
+#define EVAL(X,Y) MERGE(X,Y)
+#define GLLOCAL_SYMBOL(fun) EVAL(RETEXTURE_PREFIX, fun)
 
 /*
  * This weird hack tracks glTexImage* calls with checksum/ID 
@@ -45,19 +56,34 @@
 
 struct track_elem
 {
+/* we split between active and alive for now in order to 
+ * not have to update / reload when the underlying software uses
+ * texture caches etc. slots are re-used on !active, !alive. */
+	bool active, alive;
+
+/* translation / detection */
 	uint32_t djbhash;
 	GLint glid;
 	GLenum target;
 	GLint format;
+
+/* detect if mipmap levels are manually updated */
 	int last_level;
+
+/* detect if we have a streaming texture slot */
 	int updates;
 
 /* inbuf is copied on updates, until the number of updates in the
  * slot exceeds a certain value (or we flag that the extra cost 
  * is accepted) to differentiate between static textures and those
  * with streaming updates */
-	uint32_t* inbuf;
-	uint32_t* outbuf;
+	struct {
+		int width;
+		int height;
+		int bpp;
+		int stride;
+		uint32_t* data;
+	} inbuf, outbuf;
 };
 
 /*
@@ -72,10 +98,23 @@ struct track_bucket
 };
 
 static struct {
-	arcan_event* dqueue;
+/* states; uninitialized -> initialized -> running | suspended */
+	int running;
+	bool local_copy;
+	int block_sz;
+
 	struct track_bucket* buckets;	
-	bool running;
-} retexture_state;
+	int n_buckets;
+
+/* notification channel for discoveries / changes */	
+	arcan_event* dqueue;
+
+/* forward table for hijacked OGL functions */
+	void (*teximage2d)(GLenum, GLint, GLint, 
+		GLsizei, GLsizei, GLint, GLenum, GLenum, const GLvoid*);
+	void (*deletetextures)(GLsizei, const GLuint*);
+
+} retexctx;
 
 static bool supported_format(GLenum target, GLint format)
 {
@@ -83,14 +122,105 @@ static bool supported_format(GLenum target, GLint format)
 		(	format == GL_RGBA || GL_BGRA )); 
 }
 
+static struct track_elem* get_elem_byglid(unsigned long glid)
+{
+	for (int i=0; i < retexctx.n_buckets; i++){
+		for (int j=0; j <retexctx.buckets[i].total; j++){
+			if (retexctx.buckets[i].elems[j].glid == glid)
+				return &retexctx.buckets[i].elems[j];
+		}
+	}
+
+	return NULL;
+}
+
+static struct track_elem* get_elem_bydjbid(unsigned long id)
+{
+	for (int i=0; i < retexctx.n_buckets; i++){
+		for (int j=0; j <retexctx.buckets[i].total; j++){
+			if (retexctx.buckets[i].elems[j].djbhash == id)
+				return &retexctx.buckets[i].elems[j];
+		}
+	}
+
+	return NULL;
+}
+
+static struct track_elem* alloc_id(unsigned long id, int* bucket, int* elem)
+{
+	int dbucket = 0;
+
+	for (; dbucket < retexctx.n_buckets; dbucket++)
+		if (retexctx.buckets[dbucket].avail > 0 || 
+			retexctx.buckets[dbucket].total == 0)
+			break;
+
+/* grow pool */
+	if (dbucket == retexctx.n_buckets){
+		size_t new_c = (retexctx.n_buckets + 1) * 2;
+		struct track_bucket* newblock = realloc(retexctx.buckets, 
+			new_c * sizeof(struct track_bucket));
+
+		if (!newblock)
+			return NULL;
+
+		retexctx.buckets = newblock;
+		memset(&retexctx.buckets[dbucket], '\0', 
+			sizeof(struct track_bucket) * (new_c - retexctx.n_buckets));
+
+		retexctx.n_buckets = new_c;
+	}
+
+/* populate with cells */
+	if (retexctx.buckets[dbucket].total == 0){
+		size_t nbsz = retexctx.block_sz;
+		retexctx.block_sz *= 1.5;
+						
+		retexctx.buckets[dbucket].elems = 
+			malloc(sizeof(struct track_elem) * nbsz); 
+		
+		if (!retexctx.buckets[dbucket].elems){
+			return NULL;
+		}
+
+		memset(retexctx.buckets[dbucket].elems, '\0', 
+			sizeof(struct track_elem) * nbsz);
+
+		retexctx.buckets[dbucket].avail = nbsz;
+		retexctx.buckets[dbucket].total = nbsz;
+	}
+
+/* find first free */
+	struct track_elem* dst = NULL;
+	int slot = 0;
+	for (; slot < retexctx.buckets[dbucket].total; slot++){
+		if (!retexctx.buckets[dbucket].elems[slot].alive &&
+			!retexctx.buckets[dbucket].elems[slot].active){
+			dst = &retexctx.buckets[dbucket].elems[slot];
+			break;
+		}
+	}
+
+	assert(dst);
+
+	retexctx.buckets[dbucket].avail--;
+	dst->active = true;
+	dst->djbhash = id;
+
+	*bucket = dbucket;
+	*elem = slot;
+
+	return dst;
+}
+
 /*
  * We use hash (djb on data reversed) as a data identifier
- * across runs
+ * across runs or tracking usage patterns where the same texture
+ * will be updated in different slots 
  */ 
 static unsigned long djb_hash(const char* str, size_t nb)
 {
 	unsigned long hash = 5381;
-	int c;
 
 	while (nb)
 		hash = ((hash << 5) + hash) + str[nb--]; /* hash * 33 + c */
@@ -103,40 +233,73 @@ static unsigned long djb_hash(const char* str, size_t nb)
  * e.g. patching the existing symbol to redirect / jump here
  * and a separate stack cleanup 
  */
-void arcan_glTexImage2D(GLenum target, GLint level, GLsizei width,
-	GLsizei height, GLint border, GLenum format, GLenum type, const GLvoid* data)
+void GLLOCAL_SYMBOL(glTexImage2D) (GLenum target, GLint level, GLint iformat, 
+	GLsizei width, GLsizei height, GLint border, GLenum format, 
+	GLenum type, const GLvoid* data)
 {
-	if (!retexture_state.running || !supported_format(format)){
-		rextexture_state.teximage2d(target, level, width, 
-			height, border, format, type, data);
-		return;
-	}
+	if (!retexctx.running || !supported_format(target, format))
+		goto upload;
+
+	size_t nb = width * height * 4;
+	unsigned long id = djb_hash(data, nb); 
+
+/* 
+ * we also use texture updates as a "tick" to determine which
+ * cached textured could be discarded to not have the decay run 
+ * rampant and "leak" memory
+ */
 
 	GLint did;
 	glGetIntegerv(GL_TEXTURE_BINDING_2D, &did);
-	struct track_elem* delem = get_elem_byglid(did);
+	struct track_elem* delem = get_elem_bydjbid(id);
+
 	if (!delem){
-		delem = alloc_id( did, 
-			djb_hash((const char*) data, width * height * 4),
-			width * height * 4 );
-/* 1. copy data into delem */
-		retexture_state.teximage2d(target, level, width,
-			height, border, format, type, data);	
-	} 
-	else {
-/* 1. update local copy but do not forward */
-		return;
+		int bucket, elem;
+		delem = alloc_id(did, &bucket, &elem);
 	}
+		
+	delem->glid = did;
+
+	if (retexctx.local_copy){
+		if (delem->inbuf.data)
+			free(delem->inbuf.data);
+
+		delem->inbuf.data = malloc(nb);
+		delem->inbuf.width = width;
+		delem->inbuf.height = height;
+		delem->inbuf.data = malloc(nb);
+		delem->inbuf.stride = width * 4;
+	 	memcpy(delem->inbuf.data, data, nb);
+
+		delem->format = format;
+		delem->last_level = level;
+	}
+
+/* already have an impostor defined, don't let the upload go through */
+	if (delem->outbuf.data)
+		return;
+
+upload:
+	retexctx.teximage2d(target, level, iformat, width, 
+		height, border, format, type, data);
 }
 
-void arcan_glDeleteTextures(GLsizei n, const GLuint* textures)
+void GLLOCAL_SYMBOL(glDeleteTextures) (GLsizei n, const GLuint* textures)
 {
 /* 1. mark the block as invalid */
+	for (int i = 0; i < n; i++){
+		struct track_elem* delem = get_elem_byglid(textures[i]);
+		if (delem)
+			delem->active = false;
+	}
+
 /* 2. (optional) emit event */
-	retexture_state.deletetextures(n, textures);
+
+/* 3. forward */
+	retexctx.deletetextures(n, textures);
 }
 
-void arcan_frameserver_retexture_update(int bucket, int id,
+void arcan_retexture_update(int bucket, int id,
 	int w, int h, int bpp, int stride, void* buf)
 {
 /*
@@ -147,26 +310,37 @@ void arcan_frameserver_retexture_update(int bucket, int id,
  */ 
 }
 
-static void* fetch_texture(struct track_elem* src, 
-	int* w, int* h, int*bpp, int* stride)
-{
-	return NULL;
-}
-
 /*
  * Request a copy of the original state for a known ID
  * by its bucket, ...
  */
-void* arcan_frameserver_retexture_fetchp(int bucket, int id,
-	int* w, int* h, int* bpp, int* stride){
-i	
-	return fetch_texture(retexture_state.buckets[bucket].
+void* arcan_retexture_fetchp(int bucket, int id,
+	int* w, int* h, int* bpp, int* stride)
+{
+	return NULL; 
 }
 
-void* arcan_frameserver_retexture_fetchid(GLuint tid, int* w,
+void* arcan_retexture_fetchid(GLuint tid, int* w,
 	int* h, int* bpp, int* stride)
 {
 	return NULL;	
+}
+
+void arcan_retexture_enable()
+{
+	retexctx.running = true;
+}
+
+void arcan_retexture_disable()
+{
+	retexctx.running = false;
+}
+
+void arcan_retexture_update_shmif(
+	struct arcan_shmif_cont* inseg, 
+	struct arcan_shmif_cont* outseg)
+{
+		
 }
 
 /* 
@@ -174,11 +348,12 @@ void* arcan_frameserver_retexture_fetchid(GLuint tid, int* w,
  * emitted to this buffer. Set local to true if we should maintain
  * local copies
  */
-void arcan_frameserver_retexture_init(arcan_event* ann, bool local)
+void arcan_retexture_init(arcan_event* ann, bool local)
 {
-	retexture_state.running = false;
-	retexture_state.dqueue = ann;
-/* 
- * map up pointers to wrapper functions
- */ 
+	retexctx.running = false;
+	retexctx.dqueue = ann;
+	retexctx.block_sz = 16;
+	retexctx.local_copy = true;
+	retexctx.teximage2d = GLSYM_FUNCTION("glTexImage2D");
+	retexctx.deletetextures = GLSYM_FUNCTION("glDeleteTextures");
 }
