f471fcbe65c5aaf8bf5ed9cb0dfcdfe1822c51a2
arcan-fe@nospam <arcan-fe@nospam>
2014-04-17 13:04:59 +0000

    (core) work on CLIP_SHALLOW on non-rotated objects accidentally made part of master, disabling for now
    FossilOrigin-Name: a39a6987069d6f31b08a4e8bcf69e37e080934dfaa807f94df73914ece15b693

---
+++ platform/stub/mem.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#include <arcan_math.h>
+#include <arcan_general.h>
+
+void* arcan_alloc_mem(size_t nb,
+	enum arcan_memtypes type, enum arcan_memhint hint, enum arcan_memalign align)
+{
+	void* buf = malloc(nb);
+
+	if (!buf && (hint & ARCAN_MEM_NONFATAL) == 0)
+		arcan_fatal("arcan_alloc_mem(), out of memory.\n");
+
+	if (hint & ARCAN_MEM_BZERO)
+		memset(buf, '\0', nb);
+
+	return buf;
+}
+
+/*
+ * Allocate memory intended for read-only or 
+ * exec use (JIT, ...)
+ */
+void* arcan_alloc_fillmem(void* data,
+	size_t ds,
+	enum arcan_memtypes type, 
+	enum arcan_memhint hint, 
+	enum arcan_memalign align)
+{
+	void* buf = arcan_alloc_mem(ds, type, hint, align);
+
+	if (!buf)
+		return NULL;
+	
+	memcpy(buf, data, ds);
+	return buf;
+}
+
+void arcan_mem_free(void* inptr)
+{
+	free(inptr);
+}
+
