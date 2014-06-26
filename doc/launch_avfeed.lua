c3a4cba02bee3f3a7fc66719f441a95df6cc8c4d
arcan-fe@nospam <arcan-fe@nospam>
2014-06-26 00:56:29 +0000

    (core+doc) launch_avfeed updated to allow user-specified modestring
    FossilOrigin-Name: 89039ced073ebdadda381edbc4ef6ec376de84e7c4db459b46aafef7a165f5f7

--- doc/launch_avfeed.lua
+++ doc/launch_avfeed.lua
@@ -1,13 +1,14 @@
 -- launch_avfeed
 -- @short: Launch a customized frameserver
--- @inargs: *argstr*, *callback*
+-- @inargs: *argstr*, *avmode*, *callback*
 -- @outargs: vid, aid
--- @longdescr: This configures a frameserver to map to the frameserver/avfeed.c
--- skeleton, intended to use for customized input frameservers. An example
--- can be found in attic/avfeed_xiq.c for a quick- and dirty hack to map up
--- a third party camera API. This function is intended for specialized
--- projects where the feature-set from the other modes (decode, encode, network, libretro)
--- is insufficient.
+-- @longdescr: launch_avfeed serves two purposes, over time it should be
+-- the principal way to launch authoritative frameservers and unify the
+-- others (load_movie etc.) and allow a quick and dirty interface for testing
+-- and experimenting with custom ones through the AVFEED_LIBS AVFEED_SOURCES
+-- compile time arguments. The global environment in FRAMESERVER_MODESTRING
+-- limits the possible arguments to avmode and is defined at compile time.
+-- If *avmode* is not specified, it defaults to 'avfeed'.
 -- @group: targetcontrol
 -- @cfunction: setupavstream
 -- @related: load_movie, launch_target, define_recordtarget, net_open, net_listen
