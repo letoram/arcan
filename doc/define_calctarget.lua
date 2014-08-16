66efa5b6c1cce81fcb723a0910477a59260351f0
bjornstahl <arcan-fe@nospam>
2014-08-16 13:59:36 +0200

    (doc) updated documentation to reflect API changes

--- doc/define_calctarget.lua
+++ doc/define_calctarget.lua
@@ -12,32 +12,55 @@
 -- the following metamethods are valid on image:
 -- get(x, y, [nchannels=3]) => r, g, b,
 -- nchannels=1 => lum
+-- histogram_storage(dstvid, [nchannels=1])
+--  updated the textured storage of dstvid (must be allocated with
+--  a dimension of >= 256*nch where nc is either 1,3 or 4.
+-- frequency(bin) => r,g,b,a
+--  return non-normalized, per / channel count for a specific bin (0..255)
 --
 -- @note: The callback will be executed as part of the main loop, it is paramount that
 -- the processing done is kept to a minimum.
+-- @note: While WORLDID cannot be used directly, creating an indirect association
+-- through a null_surface and then image_sharestorage(WORLDID, null_surface) and
+-- using that as a source in vidary is entirely possible.
 -- @note: setting RENDERTARGET_DETACH as detacharg means that the object will no-longer
 -- be associated with the main rendering pipeline.
 -- @note: setting RENDERTARGET_SCALE will calculate a transform to maintain relative
 -- size and positioning to the main pipeline, even if the storage *dstvid* has different
 -- dimensions.
+-- @note: Using the same object for *dstvid* and as member of *vidary* results
+-- in undefined contents in the offscreen target.
 -- @group: targetcontrol
 -- @cfunction: procset
 -- @related: define_rendertarget, define_recordtarget, fill_surface
+function cbfun(source, w, h)
+	print(srcary:get(0, 0));
+end
+
 function main()
-#ifdef MAIN
 	dstvid = fill_surface(64, 64, 0, 0, 0, 64, 64);
 	srcvid = color_surface(64, 64, 255, 128, 64);
 
+#ifdef MAIN
 	define_calctarget(dstvid, {srcvid}, RENDERTARGET_DETACH,
-		RENDERTARGET_SCALE, 10,
-	function(srcary, w, h)
-		print(srcary:get(0, 0));
-	end);
+		RENDERTARGET_SCALE, 10, cbfun);
 
 	show_image(srcvid);
 	show_image(dstvid);
 #endif
 
 #ifdef ERROR1
+	define_calctarget(WORLDID, {srcvid}, RENDERTARGET_DETACH,
+	RENDERTARGET_SCALE, 10, cbfun);
+#endif
+
+#ifdef ERROR2
+	define_calctarget(dstvid, {srcvid, WORLDID}, RENDERTARGET_DETACH,
+	RENDERTARGET_SCALE, 10, cbfun);
+#endif
+
+#ifdef ERROR3
+	define_calctarget(dstvid, {srcvid}, RENDERTARGET_DETACH,
+		RENDERTARGET_SCALE, 10, define_calctarget);
 #endif
 end
