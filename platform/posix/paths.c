ab203c800358a1bce4a4275af41fa95749a146ea
arcan-fe@nospam <arcan-fe@nospam>
2014-08-21 22:35:41 +0000

    (platform/posix) permit NULL SYS_LIBS namespace, change evaluation order to give priority to environment variable / command-line namespace overrides
    FossilOrigin-Name: 9dc5651489e2b39adbebd23243e4ce1783d56f38484336ae9bdcf829556eed00

--- platform/posix/paths.c
+++ platform/posix/paths.c
@@ -126,6 +126,16 @@ static char* unix_find(const char* fname)
 	return res;
 }
 
+static void soft_override_namespace(
+	const char* new, enum arcan_namespaces space)
+{
+	char* tmp = arcan_expand_resource("", space);
+	if (!tmp)
+		arcan_override_namespace(new, space);
+	else
+		free(tmp);
+}
+
 /*
  * This is set-up to mimic the behavior of previous arcan
  * version as much as possible. For other, more controlled settings,
@@ -133,15 +143,20 @@ static char* unix_find(const char* fname)
  */
 void arcan_set_namespace_defaults()
 {
-	arcan_override_namespace(binpath_unix(), RESOURCE_SYS_BINS);
-	arcan_override_namespace(libpath_unix(), RESOURCE_SYS_LIBS);
+/*
+ * use environment variables as overrides (then command-line args
+ * can override those in turn)
+ */
+	for (int i = 0; i < sizeof( envvs ) / sizeof( envvs[0] ); i++){
+		const char* tmp = getenv(envvs[i]);
+		arcan_override_namespace(tmp, 1 << i);
+	}
+
+	soft_override_namespace(binpath_unix(), RESOURCE_SYS_BINS);
+	soft_override_namespace(libpath_unix(), RESOURCE_SYS_LIBS);
 
 	char* respath = unix_find("resources");
-	if (!respath){
-		arcan_warning("set_namespace_defaults(): "
-			"Couldn't find a suitable 'resources' folder.\n");
-	}
- 	else {
+	if (respath){
 		size_t len = strlen(respath);
 		char debug_dir[ len + sizeof("/logs") ];
 		char state_dir[ len + sizeof("/savestates") ];
@@ -151,10 +166,10 @@ void arcan_set_namespace_defaults()
 		snprintf(state_dir, sizeof(state_dir), "%s/savestates", respath);
 		snprintf(font_dir, sizeof(font_dir), "%s/fonts", respath);
 
-		arcan_override_namespace(respath, RESOURCE_APPL_SHARED);
-		arcan_override_namespace(debug_dir, RESOURCE_SYS_DEBUG);
-		arcan_override_namespace(state_dir, RESOURCE_APPL_STATE);
-		arcan_override_namespace(font_dir, RESOURCE_SYS_FONT);
+		soft_override_namespace(respath, RESOURCE_APPL_SHARED);
+		soft_override_namespace(debug_dir, RESOURCE_SYS_DEBUG);
+		soft_override_namespace(state_dir, RESOURCE_APPL_STATE);
+		soft_override_namespace(font_dir, RESOURCE_SYS_FONT);
 	}
 
 	char* scrpath = unix_find("appl");
@@ -162,20 +177,7 @@ void arcan_set_namespace_defaults()
 		scrpath = unix_find("themes"); /* legacy */
 
 	if (scrpath){
-		arcan_override_namespace(scrpath, RESOURCE_APPL);
-		arcan_override_namespace(scrpath, RESOURCE_APPL_TEMP);
-	}
-
-/*
- * NOTE: nice spot to add configuration file loading
- */
-
-/*
- * use environment variables as overrides (then command-line args
- * can override those in turn)
- */
-	for (int i = 0; i < sizeof( envvs ) / sizeof( envvs[0] ); i++){
-		const char* tmp = getenv(envvs[i]);
-		arcan_override_namespace(tmp, 1 << i);
+		soft_override_namespace(scrpath, RESOURCE_APPL);
+		soft_override_namespace(scrpath, RESOURCE_APPL_TEMP);
 	}
 }
