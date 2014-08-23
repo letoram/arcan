ea1f65c684e385d6a866fc54cc1f6ec0ac14a03e
arcan-fe@nospam <arcan-fe@nospam>
2014-08-23 17:13:09 +0000

    (examples) added shmif- example / skeleton
    FossilOrigin-Name: 799a5a8aad8bef16d7968600ae8ece4c4a29ed45c73e17fa500bd49c50b5ce18

---
+++ examples/shmif-conn/src/main.c
@@ -0,0 +1,60 @@
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdbool.h>
+
+#include <arcan_shmif.h>
+
+int main(int argc, char** argv)
+{
+	char* resource = getenv("ARCAN_ARG");
+	char* keyfile = NULL;
+
+	if (getenv("ARCAN_CONNPATH")){
+		keyfile = arcan_shmif_connect(
+			getenv("ARCAN_CONNPATH"), getenv("ARCAN_CONNKEY"));
+	}
+	else {
+		LOG("No arcan-shmif connection, check ARCAN_CONNPATH environment.\n\n");
+		return EXIT_FAILURE;
+	}
+
+	if (!keyfile){
+		LOG("No valid connection key found, giving up.\n");
+		return EXIT_FAILURE;
+	}
+
+	struct arcan_shmif_cont ctx = arcan_shmif_acquire(
+		keyfile, SHMIF_INPUT, true, false);
+
+	if (resource){
+		struct arg_arr* arr = arg_unpack(resource);
+/*
+ *  implement additional argument passing
+ *  use arg_lookup(arr, sym, ind, dptr) => [true | false]
+ */
+	}
+
+	if (!ctx.addr){
+		LOG("Couldn't acquire a valid shared memory segment, giving up.\n");
+		return EXIT_FAILURE;
+	}
+
+	arcan_event ev;
+
+	bool running = true;
+
+	while (running && arcan_event_wait(&ctx, &ev)){
+		if (ev.category == EVENT_TARGET)
+		switch (ev.kind){
+		case TARGET_COMMAND_EXIT:
+			running = false;
+		break;
+		}
+	}
+
+cleanup:
+	return EXIT_SUCCESS;
+}
+
