607acd8c00f8a9699f4d6498accbd65f62a3e29a
arcan-fe@nospam <arcan-fe@nospam>
2014-09-14 21:14:22 +0000

    (doc) added interpolation argument to transformations
    FossilOrigin-Name: 19050e12467f2a81df656e0b6642709958980a4982c243f807f0cc243391fd28

--- doc/resize_image.lua
+++ doc/resize_image.lua
@@ -1,9 +1,11 @@
 -- resize_image
 -- @short: Change object dimensions based on absolute pixel values.
--- @inargs: vobj, new_w, new_h, *time*
+-- @inargs: vobj, new_w, new_h, *time*, *interp*
 -- @longdescr: This transformation, internally remapped to the
 -- scale_image call but with scale values based on the initial dimensions
 -- of the object, sets a new desired output dimension.
+-- Interp can be set to one of the constants (INTERP_LINEAR,
+-- INTERP_SINE, INTERP_EXPIN, INTERP_EXPOUT, INTERP_EXPINOUT).
 -- @note: the end dimensions can still be manipulated through the
 -- vertex shader stage, but such changes do not affect other engine
 -- features (like picking and other forms of collision detection).
--- doc/scale_image.lua
+++ doc/scale_image.lua
@@ -1,10 +1,12 @@
 -- scale_image
 -- @short: Set the current scale-factor for the specified image.
--- @inargs: vid, sx, sy, *dt*
+-- @inargs: vid, sx, sy, *time*, *interp*
 -- @longdescr: Alter the scale transform for *vid*. These values
 -- are relative to the initial dimensions for the object in question,
 -- while function such as resize_image internally converts the absolute
 -- values to relative scale.
+-- Interp can be set to one of the constants (INTERP_LINEAR,
+-- INTERP_SINE, INTERP_EXPIN, INTERP_EXPOUT, INTERP_EXPINOUT).
 -- @group: image
 -- @cfunction: arcan_lua_scaleimage
 -- @related: resize_image
