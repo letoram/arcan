2563a282f2f0fae831d088278648efae27ba0721
bjornstahl <arcan-fe@nospam>
2014-09-26 21:28:36 +0200

    (lua) added open_nonblock for non-blocking text/file based input

---
+++ doc/open_nonblock.lua
@@ -0,0 +1,28 @@
+-- open_nonblock
+-- @short: Open a file in read-only mode for non-blocking reads.
+-- @inargs: res
+-- @outargs: blocktbl
+-- @longdescr: Locate and open the resource indicated by *res* and
+-- map to a usertable (blocktbl, methods: read, close) for non-blocking
+-- buffered reads. Calls to read (no arguments) will yield nil or a
+-- string representing a line or null-terminated buffer contents.
+-- @note: the corresponding file-descriptor will be closed upon
+-- garbage collection or by calling the table- method close.
+-- @group: resource
+-- @cfunction: opennonblock
+-- @related:
+#ifdef MAIN
+function main()
+	a = open_nonblock("test.txt")
+	if (a == nil) then
+		return shutdown("couldn't open test.txt");
+	end
+end
+
+function main_clock_pulse()
+	local line = a:read();
+	if (line ~= nil) then
+		print(line);
+	end
+end
+#endif
