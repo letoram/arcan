c8c08c33bd57c6cf55bb4e0b02f51e18446d2e17
arcan-fe@nospam <arcan-fe@nospam>
2014-09-27 11:57:59 +0000

    (doc) fixed set_image_as_frame description
    FossilOrigin-Name: f3b056fea930608d2fe090a436099fb695e3b3f16d3be6cb23426cb3a49d81dd

--- doc/set_image_as_frame.lua
+++ doc/set_image_as_frame.lua
@@ -1,10 +1,14 @@
 -- set_image_as_frame
 -- @short: Specify the contents of a multi-frame object.
--- @inargs: contvid, srcvid, index
+-- @inargs: contvid, srcvid, index, *detach*
 -- @longdescr: Objects can be turned into multi-frame objects by calling
 -- image_framesetsize which will allocate a static number of slots for
 -- attaching objects. By default, these refer back to the source object,
--- but each slot can be filled with an object using the set_image_as_frame function.
+-- but each slot can be filled with an object using the set_image_as_frame
+-- function. If detach is set to FRAMESET_NODETACH (default) srcvid will
+-- behave as a normal object. If detach is set to FRAMESET_DETACH however,
+-- its hierarchical parent will be set to contvid and will no-longer be
+-- rendered on its own.
 -- @group: image
 -- @cfunction: arcan_lua_imageasframe
 -- @related: image_framesetsize, image_framecyclemode, image_active_frame
