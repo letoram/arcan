92db4bca30c567471e970e3ca1c0a6062b205ee5
bjornstahl <arcan-fe@nospam>
2014-10-25 13:39:43 +0200

    (doc+lua) corrected launch/config documentation

---
+++ src/platform/stub/launch.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#include PLATFORM_HEADER
+
+int arcan_target_launch_external(
+	const char* fname, char** argv, char** envv, char** libs)
+{
+	return EXIT_FAILURE;
+}
+
+struct arcan_frameserver* arcan_target_launch_internal(
+	const char* fname, char** argv, char** env, char** libs)
+{
+	return NULL;
+}
