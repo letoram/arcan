5e9a119169ba5aa04a692c8c725bdc2808d90b58
arcan-fe@nospam <arcan-fe@nospam>
2014-10-27 15:53:41 +0000

    (benchmark) added two stencil clipping benchmarks
    FossilOrigin-Name: 4866e860b7d9e27d80114428dc04cf3a009ec8ccd377b1b05374cac652f521bc

---
+++ tests/benchmark/stencil/stencil.lua
@@ -0,0 +1,36 @@
+--
+-- Hierarchical stencil (depth)
+--
+
+function stencil()
+	system_load("scripts/benchmark.lua")();
+	benchmark_setup( arguments[1] );
+
+	local root = fill_surface(VRESW * 0.5, VRESH * 0.5, 255, 0, 0);
+	move_image(root, VRESW * 0.25, VRESH * 0.25);
+	show_image(root);
+
+	last = fill_surface(VRESW * 0.5, VRESH * 0.5, 0, 255, 0);
+	move_image(last, math.random(10), math.random(10));
+	link_image(last, root);
+	image_clip_on(last, CLIP_ON);
+	show_image(last);
+
+	benchmark = benchmark_create(40, 5, 10, fill_step);
+end
+
+function fill_step()
+	local new = fill_surface(VRESW * 0.5, VRESH * 0.5, math.random(255),
+		math.random(255), math.random(255));
+	rotate_image(new, math.random(359));
+	show_image(new);
+	link_image(new, last);
+	image_clip_on(new);
+	last = new;
+end
+
+function stencil_clock_pulse()
+	if (not benchmark:tick()) then
+		return shutdown();
+	end
+end
---
+++ tests/benchmark/stencilflat/stencilflat.lua
@@ -0,0 +1,30 @@
+--
+-- Horizontal stencil (individual pairs)
+--
+function stencilflat()
+	system_load("scripts/benchmark.lua")();
+	benchmark_setup( arguments[1] );
+	benchmark = benchmark_create(40, 5, 10, fill_step);
+end
+
+function fill_step()
+	local parent = fill_surface(VRESW * 0.5, VRESH * 0.5,
+		math.random(255), math.random(255), math.random(255));
+
+	local child = fill_surface(VRESW * 0.5, VRESH * 0.5,
+		math.random(255), math.random(255), math.random(255));
+
+	move_image(child, math.random(64), math.random(64));
+	rotate_image(child, 64);
+	link_image(child, parent);
+	image_clip_on(child, CLIP_ON);
+
+	show_image({parent, child});
+	return parent;
+end
+
+function stencilflat_clock_pulse()
+	if (not benchmark:tick()) then
+		return shutdown();
+	end
+end
