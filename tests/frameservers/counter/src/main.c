dfcb890df6b5a0bca33a053f8068744a6b22678a
bjornstahl <arcan-fe@nospam>
2014-11-06 07:11:47 +0100

    (tests) added counter frameserver
    Added a simple testing frameserver that
    increments RGB (b -> g -> r) with wraparound
    per channel every frame. It can be built
    as the avfeed frameserver or connect
    authoritatively through the database or
    non-authoritatively through the ARCAN_CONNPATH
    mechanism.

---
+++ tests/frameservers/counter/src/main.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdbool.h>
+
+#include <arcan_shmif.h>
+
+#ifdef ENABLE_FSRV_AVFEED
+void arcan_frameserver_avfeed_run(const char* resource, const char* keyfile)
+#else
+int main(int argc, char** argv)
+#endif
+{
+	struct arg_arr* aarr;
+	struct arcan_shmif_cont cont = arcan_shmif_open(
+		SEGID_APPLICATION, SHMIF_ACQUIRE_FATALFAIL, &aarr);
+
+	arcan_event ev;
+	bool running = true;
+
+	arcan_shmif_resize(&cont, 640, 480);
+
+	uint8_t step_r = 0;
+	uint8_t step_g = 0;
+	uint8_t step_b = 0;
+
+	while(running){
+	for (size_t row = 0; row < cont.addr->h; row++)
+		for (size_t col = 0; col < cont.addr->w; col++)
+			cont.vidp[ row * cont.addr->w + col ] = RGBA(step_r, step_g, step_b, 0xff);
+			step_b++;
+			step_g += step_b == 255;
+			step_r += step_r == 255;
+
+		arcan_shmif_signal(&cont, SHMIF_SIGVID);
+
+		while (arcan_event_poll(&cont.inev, &ev) == 1){
+			if (ev.category == EVENT_TARGET)
+			switch (ev.kind){
+			case TARGET_COMMAND_EXIT:
+				running = false;
+			break;
+			}
+		}
+	}
+
+#ifndef ENABLE_FSRV_AVFEED
+	return EXIT_SUCCESS;
+#endif
+}
