ea04845419c4563dde82aee0bdd74fc3ae57f957
arcan-fe@nospam <arcan-fe@nospam>
2014-11-09 15:19:47 +0000

    (core+) refactor towards dropping GL_HEADERS hack
    This will break several builds, but removing
    the dependency to GLEW and started dropping the
    need for GL_HEADERS throughout the project.
    
    Added the option for docgen to generate
    a vim highlights file, and doc->test generation
    almost completed.
    
    Shortened the shmpuncher testcase.
    
    FossilOrigin-Name: 7407ae574d26ad8fac64f8eb624b546478c5e13f9381f5ba37831c6f57b62680

--- tests/security/shmpuncher/shmpuncher.c
+++ tests/security/shmpuncher/shmpuncher.c
@@ -12,22 +12,13 @@ int main(int argc, char** argv)
 	struct arcan_shmif_cont cont = arcan_shmif_open(
 		SEGID_APPLICATION, SHMIF_ACQUIRE_FATALFAIL, &aarr);
 
-	while(1){
-		size_t neww = rand() % PP_SHMPAGE_MAXW;
-		size_t newh = rand() % PP_SHMPAGE_MAXH;
-
-		arcan_shmif_resize(&cont, neww, newh);
-
-	for (size_t row = 0; row < cont.addr->h; row++)
-		for (size_t col = 0; col < cont.addr->w; col++)
-			cont.vidp[ row * cont.addr->w + col ] = RGBA(255, 0, 0, 255);
+	arcan_shmif_resize(&cont, 640, 480);
 
 /* truncate to mess with parent */
-		ftruncate(cont.shmh, 1024);
+	ftruncate(cont.shmh, 1024);
 
 /* make sure it'll try to copy */
-		arcan_shmif_signal(&cont, SHMIF_SIGVID);
-	}
+	arcan_shmif_signal(&cont, SHMIF_SIGVID);
 
 	return EXIT_SUCCESS;
 }
