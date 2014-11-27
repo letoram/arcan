26d44db2922075a96dc65bc3a285df0b8bbd8d6c
bjornstahl <arcan-fe@nospam>
2014-11-27 19:00:53 +0100

    (platform/posix) glob namespacing fix
    This is a fix for the issue where multiple namespaces
    are mapped to the same path and still get traversed
    (meaning that we will get duplicate results).

--- src/platform/posix/glob.c
+++ src/platform/posix/glob.c
@@ -10,6 +10,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <glob.h>
+#include <math.h>
 
 #include <stdbool.h>
 #include <sys/types.h>
@@ -24,25 +25,52 @@ unsigned arcan_glob(char* basename, enum arcan_namespaces space,
 	if (!basename || verify_traverse(basename) == NULL)
 		return 0;
 
-	for (int i = 1; i <= RESOURCE_SYS_ENDM; i <<= 1){
+/* need to track so that we don't glob namespaces that
+ * happen to collide */
+	size_t nspaces = (size_t) log2(RESOURCE_SYS_ENDM);
+
+	char* globslots[ nspaces ];
+	memset(globslots, '\0', sizeof(globslots));
+	size_t ofs = 0;
+
+	for (size_t i = 1; i <= RESOURCE_SYS_ENDM; i <<= 1){
 		if ( (space & i) == 0 )
 			continue;
 
 		glob_t res = {0};
 		char* path = arcan_expand_resource(basename, i);
+		bool match = false;
+
+		for (size_t i = 0; i < ofs; i++){
+			if (globslots[ i ] == NULL)
+				break;
+
+			if (strcmp(path, globslots[i]) == 0){
+				arcan_mem_free(path);
+				match = true;
+			}
+		}
+
+		if (match)
+			continue;
+
+		globslots[ofs++] = path;
 
 		if ( glob(path, 0, NULL, &res) == 0 ){
 			char** beg = res.gl_pathv;
+
 			while(*beg){
 				cb(strrchr(*beg, '/') ? strrchr(*beg, '/')+1 : *beg, tag);
 				beg++;
 				count++;
 			}
-		}
 
-		globfree(&res);
-		arcan_mem_free(path);
+			globfree(&res);
+		}
 	}
+
+	for (size_t i = 0; i < nspaces && globslots[i] != NULL; i++)
+		arcan_mem_free(globslots[i]);
 
 	return count;
 }
