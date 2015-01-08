2eecebb09166049209b975e3d79104e8d4e997f0
arcan-fe@nospam <arcan-fe@nospam>
2015-01-08 19:21:59 +0000

    (core/platform) descriptor passing investigation
    Minor changes to investigate an issue where
    a subsegment is created correctly, and the socketpair
    push/fetch across the connection socket seem to
    work correctly but a -1 descriptor is ultimately
    received.
    
    FossilOrigin-Name: 11da8ad76ee569b5cd8f3dc17c77c9a93e53d06220e65375bb26d356e356bb18

--- src/platform/posix/fdpassing.c
+++ src/platform/posix/fdpassing.c
@@ -6,6 +6,7 @@
 #include <sys/un.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <errno.h>
 #include <stdint.h>
 #include "../platform.h"
 
@@ -50,42 +51,52 @@ bool arcan_pushhandle(file_handle source, int channel)
 
 file_handle arcan_fetchhandle(int sockin_fd)
 {
-	int rv = -1;
+	if (sockin_fd == -1)
+		return -1;
 
-/* some would call this black magic. They'd be right. */
-	if (sockin_fd != -1){
-		char empty;
+	char empty;
 
-		struct cmsgbuf {
-			struct cmsghdr hdr;
-			int fd[1];
-		} msgbuf;
+	struct cmsgbuf {
+		struct cmsghdr hdr;
+		int fd[1];
+	} msgbuf;
 
-		struct iovec nothing_ptr = {
-			.iov_base = &empty,
-			.iov_len = 1
-		};
+	struct iovec nothing_ptr = {
+		.iov_base = &empty,
+		.iov_len = 1
+	};
 
-		struct msghdr msg = {
-			.msg_name = NULL,
-			.msg_namelen = 0,
-			.msg_iov = &nothing_ptr,
-			.msg_iovlen = 1,
-			.msg_flags = 0,
-			.msg_control = &msgbuf,
-			.msg_controllen = sizeof(struct cmsghdr) + sizeof(int)
-		};
+	struct msghdr msg = {
+		.msg_name = NULL,
+		.msg_namelen = 0,
+		.msg_iov = &nothing_ptr,
+		.msg_iovlen = 1,
+		.msg_flags = 0,
+		.msg_control = &msgbuf,
+		.msg_controllen = sizeof(struct cmsghdr) + sizeof(int)
+	};
 
-		struct cmsghdr* cmsg = CMSG_FIRSTHDR(&msg);
-		cmsg->cmsg_len = msg.msg_controllen;
-		cmsg->cmsg_level = SOL_SOCKET;
-		cmsg->cmsg_type  = SCM_RIGHTS;
-		int* dfd = (int*) CMSG_DATA(cmsg);
-		*dfd = -1;
+	struct cmsghdr* cmsg = CMSG_FIRSTHDR(&msg);
+	cmsg->cmsg_len = msg.msg_controllen;
+	cmsg->cmsg_level = SOL_SOCKET;
+	cmsg->cmsg_type  = SCM_RIGHTS;
+	int* dfd = (int*) CMSG_DATA(cmsg);
+	*dfd = -1;
 
-		if (recvmsg(sockin_fd, &msg, MSG_DONTWAIT | MSG_NOSIGNAL) >= 0)
-			rv = msgbuf.fd[0];
+#ifdef NONBLOCK_RECV
+	if (-1 == recvmsg(sockin_fd, &msg, MSG_DONTWAIT | MSG_NOSIGNAL))
+#else
+	if (-1 == recvmsg(sockin_fd, &msg, MSG_NOSIGNAL))
+#endif
+	{
+		fprintf(stderr, "posix/fdpassing.c(), couldn't receive handle: %d, %s\n",
+			errno, strerror(errno));
+		return -1;
 	}
 
-	return rv;
+	int nd = msgbuf.fd[0];
+	if (-1 == nd)
+		fprintf(stderr, "posix/fdpassing.c(), empty handle received\n");
+
+	return nd;
 }
