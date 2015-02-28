c10b4ef05428619849692ea0cefd7ebe0ef1c5bc
bjornstahl <arcan-fe@nospam>
2015-02-28 22:55:15 +0100

    (core) defer higher-level resize to first frame
    Previously, as soon as a frameserver got a resize
    request approved, the corresponding event was sent
    and the video backing store populated with an
    empty frame. This have lead to an initial "black flicker"
    on highly visible resizes. With this commit, the
    resize event and backing store resize is connected to
    the push_buffer step (thus when a frameserver has
    delivered a frame).

--- src/engine/arcan_frameserver.h
+++ src/engine/arcan_frameserver.h
@@ -236,7 +236,7 @@ void arcan_frameserver_dropsemaphores_keyed(char*);
  * is intact and look for any state-changes, e.g. resize (which would
  * require a recalculation of shared memory layout */
 void arcan_frameserver_tick_control(arcan_frameserver*);
-bool arcan_frameserver_resize(shm_handle*, int, int);
+bool arcan_frameserver_resize(arcan_frameserver*, int, int);
 
 /* default implementations for shared memory framequeue readers,
  * two with separate sync (vidcb audcb) and one where sync is
