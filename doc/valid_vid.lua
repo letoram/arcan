50008b5940d2f310b6bf19f4536167c89437f37c
bjornstahl <arcan-fe@nospam>
2015-05-16 01:57:28 +0200

    (lua) extended valid_vid calls with subtype

--- doc/valid_vid.lua
+++ doc/valid_vid.lua
@@ -1,7 +1,14 @@
 -- valid_vid
 -- @short: Check if vid matches a valid object or not, also accepts NULL/BADID arguments.
--- @inargs: *vid*
--- @outargs: validbool
+-- @inargs: *vid*, *type*
+-- @outargs: true or false
+-- @longdescr: Due to the design decision to use a numerical type to reference
+-- the internal engine type most visible entities, a number of situations arise where
+-- one might need to test if a specific numeric value maps to a corresponding engine
+-- object before calling some functions that require a valid VID or even a valid VID
+-- that is also connected to a frameserver or other secondary processing types.
+-- @note: Possible values for *type* are TYPE_FRAMESERVER and TYPE_3DOBJECT.
+-- requirement
 -- @group: vidsys
 -- @cfunction: validvid
 -- @flags:
