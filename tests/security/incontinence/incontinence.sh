757d3353fca2474c66d2aad146d34a5adf8285aa
arcan-fe@nospam <arcan-fe@nospam>
2015-05-27 21:30:45 +0000

    (tests) added incontinence to provoke leaks
    There are a handful of IPC primitives that have been prone to leaking
    (with possible resource exhaustion) in the past (and in upcoming fixes):
    
     [semaphores (arcan_pid_rnd_[a,v,e])]
     [socket (domain)]
     [thread-stack (nanny- in parent)]
     [shared mapping (arcan_shm)]
    
    With different behavior for authoritative vs non-authoritative
    connections. This test approaches this by spamming / logging both
    avfeed- connections and external ones.
    
    FossilOrigin-Name: 644c679547eceddb7e7939277212c4eaca41804b0859103f81555d2f0c967349

---
+++ tests/security/incontinence/incontinence.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+arcan -W headless -d :memory: -p ./ ../incontinence &> /dev/null &
+
+sleep 1
+
+handler(){
+	PID=`cat incontinence.pid`
+	rm incontinence.pid
+	echo "done, killing $PID"
+	kill $PID
+	exit
+}
+
+trap handler INT TERM
+
+export ARCAN_CONNPATH="incontinence"
+COUNTER=1
+while :
+do
+	./incontinence
+	echo " -- RUN `date` -- " >> incontinence.log
+	PID=`cat incontinence.pid`
+	cat /proc/$PID/maps >> incontinence.log
+	echo "`cat /proc/$PID/maps |wc -l` mappings "
+	ls -l /proc/$PID/fd >> incontinence.log
+	echo "`ls -l /proc/$PID/fd |wc -l` files "
+	ls -l /dev/shm/ |grep arcan >> incontinence.log
+	echo "`ls -l /dev/shm | grep arcan | wc -l` shared entries"
+done
