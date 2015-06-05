ee7ea05278cd0b6cdc1efc3145ba7b2c95ba1b5a
arcan-fe@nospam <arcan-fe@nospam>
2015-06-05 13:38:46 +0000

    (doc) corrected link-image and mangen naming
    FossilOrigin-Name: 28ef7758656675245198876320381a64002da5ddfae7812a15cd51d0f49b9d4a

--- doc/link_image.lua
+++ doc/link_image.lua
@@ -1,14 +1,23 @@
 -- link_image
--- @short: Bind the state-space of one video object to that of another.
+-- @short: Change some properties to be automatically relative to another object.
 -- @inargs: src, newparent, *anchor*
--- @longdescr: Full video objects (not instances and other restricted types)
--- can have their properties be dependent of the state of another. This function
--- associates *src* with *newparent*, which by default means that opacity, position
--- orientation becomes relative. The exact properties can be tuned with the _mask class
--- of functions. If anchor is specified to any of the values:
--- ANCHOR_UR, ANCHOR_LL, ANCHOR_LR, ANCHOR_C
--- the position relative to a parent will be based on a different
--- (non rotated) anchor of the parent (default is upper-left corner, UL).
+-- @longdescr: By default, all objects defines their properties relative to
+-- WORLDID, but this association can be reassigned by calling *link_image* for
+-- full video objects.  A full video object is one that do is not a clone
+-- (instance) and have not been marked as persistant. The default properties
+-- that are turned relatrive in this way are *opacity*, *position*,
+-- *orientation*, though these can be controlled with mask functions such as
+-- ref:image_mask_set. *src* refers to the object that should be reassigned and
+-- *newparent* refers to the object that the assignment would be changed to.
+-- This also applies to life tracking, delete or expire *newparent* and this
+-- will cascade to any associated objects. Both *src* and *newparent* must be
+-- full video objects.  The optional *anchor* properties can be one of the
+-- following values: ANCHOR_UL, ANCHOR_UR, ANCHOR_LL, ANCHOR_LR, ANCHOR_C and
+-- refers to the anchor point, meaning that if the *newparent* would change
+-- size, the position of *src* will be shifted in to reflect this based on the
+-- anchor point. The default anchor point is ANCHOR_UL.
+-- @note: Rotation/Orientation do not apply to the anchor point.
+-- @note: Link to self is equivalent to linking back to WORLDID.
 -- @group: image
 -- @cfunction: linkimage
 function main()
