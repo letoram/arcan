8a92396f84e41e2c93dff92b99ae88e1d76320d9
bjornstahl <arcan-fe@nospam>
2015-08-06 16:55:12 +0200

    (db/tool/lua) match_keys to expose appl_ domain
    dbtool- previously only stubbed the ability to dump the appl- specific
    K/V pairs that was set (no appl- needed / used the feature at that
    point). This patch fixes that, along with setting the default for
    match_keys to search the APPL_ domain rather than explicitly supporting
    just DVT_TARGET, DVT_CONFIG.

--- doc/match_keys.lua
+++ doc/match_keys.lua
@@ -1,17 +1,30 @@
 -- match_keys
--- @inargs: pattern, domain
+-- @inargs: pattern, *domain*
 -- @short: Return target:value for keys that match the specified pattern
 -- @outargs: strtbl
--- @longdescr: This function can be used to retrieve multiple domain:value entries
--- where domain:key=value matches for some specific pattern for key (% is used as wildcard).
--- The *domain* argument can be set to KEY_CONFIG or to KEY_TARGET.
+-- @longdescr: This function can be used to retrieve multiple domain:value
+-- entries where domain:key=value matches for some specific pattern for key
+-- (where % is used as wildcard, SQL style).
+-- The optional *domain* argument can be set to KEY_CONFIG or to KEY_TARGET
+-- but defaults to the appl- specific key-value store.
 -- @group: database
 -- @cfunction: matchkeys
 -- @related:
 function main()
 #ifdef MAIN
+	for i,v in ipairs(match_keys("%", KEY_CONFIG)) do
+		print(i, v);
+	end
+
+	for i,v in ipairs(match_keys("%")) do
+		local pos, stop = string.find(v, "=", 1);
+		local key = string.sub(v, 1, pos - 1);
+		local val = string.sub(v, stop + 1);
+		print(key, val);
+	end
 #endif
 
 #ifdef ERROR1
+	print(match_keys("%", BADID)[1]);
 #endif
 end
