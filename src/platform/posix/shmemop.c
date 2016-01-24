d59853b31b8affe46539588247de5f61105364d8
arcan-fe@nospam <arcan-fe@nospam>
2016-01-24 10:12:03 +0000

    (shmif+) moved some code shmif->platform
    FossilOrigin-Name: ba22b8c286847ebbd01742657380b8779acce0a02c226c0e5ce31759224f6342

---
+++ src/platform/posix/shmemop.c
@@ -0,0 +1,33 @@
+#include <stdint.h>
+#include "arcan_shmif.h"
+
+static inline void* align64(uint8_t* inptr)
+{
+	return (void*) (((uintptr_t)inptr % 64 != 0) ?
+		inptr + 64 - ((uintptr_t) inptr % 64) : inptr);
+}
+
+uintptr_t arcan_shmif_mapav(
+	struct arcan_shmif_page* addr,
+	shmif_pixel* vbuf[], size_t vbufc, size_t vbuf_sz,
+	shmif_asample* abuf[], size_t abufc, size_t abuf_sz)
+{
+/* now we are in bat county */
+	uint8_t* wbuf = (uint8_t*)addr + sizeof(struct arcan_shmif_page);
+
+	for (size_t i = 0; i < abufc; i++){
+			wbuf = align64(wbuf);
+			if (abuf)
+				abuf[i] = abuf_sz ? (shmif_asample*) wbuf : NULL;
+			wbuf += abuf_sz;
+	}
+
+	for (size_t i = 0; i < vbufc; i++){
+			wbuf = align64(wbuf);
+			if (vbuf)
+				vbuf[i] = vbuf_sz ? (shmif_pixel*) align64(wbuf) : NULL;
+			wbuf += vbuf_sz;
+		}
+
+	return (uintptr_t) wbuf - (uintptr_t) addr;
+}
