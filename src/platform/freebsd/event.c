7f0ae96e2d57fc67089503dd0fe9be31c8b0f7a0
bjornstahl <arcanfe@nospam>
2016-06-05 12:36:53 +0200

    (build) FreeBSD console/mouse driver preparations
    After a weekend of testing, it seems that the KMS support in FBSD is good
    enough to start adding support for a local / terminal input platform to
    match. This push renames the 'linux' input platform to the more fitting 'evdev'
    (as it also works on DragonFlyBSD) and adds a stub for FBSD console.

---
+++ src/platform/freebsd/event.c
@@ -0,0 +1,74 @@
+/*
+ * No copyright claimed, Public Domain
+ */
+
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <assert.h>
+#include <unistd.h>
+
+#include "arcan_shmif.h"
+#include "arcan_math.h"
+#include "arcan_general.h"
+#include "arcan_event.h"
+
+arcan_errc platform_event_analogstate(int devid, int axisid,
+	int* lower_bound, int* upper_bound, int* deadzone,
+	int* kernel_size, enum ARCAN_ANALOGFILTER_KIND* mode)
+{
+    return ARCAN_OK;
+}
+
+void platform_event_analogall(bool enable, bool mouse)
+{
+}
+
+void platform_event_analogfilter(int devid,
+	int axisid, int lower_bound, int upper_bound, int deadzone,
+	int buffer_sz, enum ARCAN_ANALOGFILTER_KIND kind)
+{
+}
+
+void platform_event_process(arcan_evctx* ctx)
+{
+}
+
+void platform_event_keyrepeat(arcan_evctx* ctx, unsigned int rate)
+{
+}
+
+void platform_event_rescan_idev(arcan_evctx* ctx)
+{
+}
+
+const char* platform_event_devlabel(int devid)
+{
+	return NULL;
+}
+
+static char* envopts[] = {
+	NULL
+};
+
+const char** platform_input_envopts()
+{
+	return (const char**) envopts;
+}
+
+void platform_event_deinit(arcan_evctx* ctx)
+{
+}
+
+void platform_event_reset(arcan_evctx* ctx)
+{
+	platform_event_deinit(ctx);
+}
+
+void platform_device_lock(int devind, bool state)
+{
+}
+
+void platform_event_init(arcan_evctx* ctx)
+{
+}
