16183c77584f4d555c42e44217800afd72d127cf
arcanfe@nospam <arcanfe@nospam>
2016-06-26 20:47:22 +0000

    (hijack/sdl12) fixes for running without an active X server
    this fixes some input scenarios with cursor locking, and initialization
    for platform combinations where there aren't an active X server. This
    approach still doesn't cover GL, but initial tests show that it should
    be fairly trivial.
    
    FossilOrigin-Name: 64bc534f51448ab505d1eb5424da4ebf4ecd4aef1989ebedea0fe9eaba00c25d

--- src/hijack/sdl12/sdl12.h
+++ src/hijack/sdl12/sdl12.h
@@ -2,24 +2,16 @@
  * No copyright claimed, Public Domain
  */
 
-/*
- * Used within the arcan_target.c / arcan_target_platform.c
- * parts of the hijack library.
- *
- * Just a list of "forward functions" for symbols that should be hijacked
- * and a table that maintains the original function pointers
- */
-
 #ifndef HAVE_SDL12
 #define HAVE_SDL12
 
 SDL_GrabMode ARCAN_SDL_WM_GrabInput(SDL_GrabMode mode);
-void ARCAN_target_init();
 void ARCAN_target_shmsize(int w, int h, int bpp);
 int ARCAN_SDL_OpenAudio(SDL_AudioSpec *desired, SDL_AudioSpec *obtained);
 SDL_Surface* ARCAN_SDL_CreateRGBSurface(Uint32 flags, int width, int height,
 	int depth, Uint32 Rmask, Uint32 Gmask, Uint32 Bmask, Uint32 Amask);
 SDL_Surface* ARCAN_SDL_SetVideoMode(int w, int h, int ncps, Uint32 flags);
+int ARCAN_SDL_VideoInit(const char*, Uint32);
 int ARCAN_SDL_PollEvent(SDL_Event* inev);
 int ARCAN_SDL_Flip(SDL_Surface* screen);
 void ARCAN_SDL_UpdateRect(SDL_Surface* screen,
@@ -31,16 +23,19 @@ int ARCAN_SDL_UpperBlit(SDL_Surface* src, const SDL_Rect* srcrect,
 void ARCAN_SDL_GL_SwapBuffers();
 void ARCAN_glFinish();
 void ARCAN_glFlush();
+void build_forwardtbl();
 
 struct hijack_fwdtbl {
 	void (*sdl_swapbuffers)(void);
 	SDL_Surface* (*sdl_setvideomode)(int, int, int, Uint32);
 	int (*sdl_pollevent)(SDL_Event*);
+	int (*sdl_waitevent)(SDL_Event*);
 	int (*sdl_pushevent)(SDL_Event*);
 	int (*sdl_peepevents)(SDL_Event*, int, SDL_eventaction, Uint32);
 	int (*sdl_openaudio)(SDL_AudioSpec*, SDL_AudioSpec*);
 	SDL_GrabMode (*sdl_grabinput)(SDL_GrabMode);
 	int (*sdl_iconify)(void);
+	void (*sdl_videoinit)(const char* driver, Uint32 flags);
 	void (*sdl_updaterect)(SDL_Surface*, Sint32, Sint32, Uint32, Uint32);
 	void (*sdl_updaterects)(SDL_Surface*, int, SDL_Rect*);
 	int (*sdl_upperblit)(SDL_Surface *src, const SDL_Rect *srcrect,
@@ -49,7 +44,7 @@ struct hijack_fwdtbl {
 	SDL_Surface* (*sdl_creatergbsurface)(Uint32, int, int,
 		int, Uint32, Uint32, Uint32, Uint32);
 	int (*audioproxy)(int, int);
-
+	int (*sdl_starteventloop)(Uint32);
 	void (*glLineWidth)(float);
 	void (*glPointSize)(float);
 	void (*glFinish)(void);
