9281e7b3dd61dc28903105724fa36b243180d601
arcan-fe@nospam <arcan-fe@nospam>
2016-11-01 15:09:32 +0000

    (shmifext/x11) readback fix
    FossilOrigin-Name: 0e3bd53fb672fbcc7c2c9bbf81149394f59c39281fd8eff813a51f4acda8d123

--- src/shmif/x11/x11.c
+++ src/shmif/x11/x11.c
@@ -1,4 +1,5 @@
 #define WANT_ARCAN_SHMIF_HELPER
+#define AGP_ENABLE_UNPURE
 #include "../arcan_shmif.h"
 #include "../shmif_privext.h"
 
@@ -14,18 +15,23 @@ struct shmif_ext_hidden_int {
 	GLXContext ctx;
 	XVisualInfo* vi;
 	Display* display;
+	struct agp_rendertarget* rtgt;
+	struct storage_info_t vstore;
+	struct agp_fenv fenv;
+	bool managed;
 	Window wnd;
 };
 
 struct arcan_shmifext_setup arcan_shmifext_defaults(
 	struct arcan_shmif_cont* con)
 {
-return (struct arcan_shmifext_setup){
-	.red = 8, .green = 8, .blue = 8,
-	.alpha = 0, .depth = 0,
-	.api = API_OPENGL,
-	.major = 2, .minor = 1
-};
+	return (struct arcan_shmifext_setup){
+		.red = 8, .green = 8, .blue = 8,
+		.alpha = 0, .depth = 24,
+		.api = API_OPENGL,
+		.builtin_fbo = true,
+		.major = 2, .minor = 1
+	};
 }
 
 static void x11_drop(struct arcan_shmif_cont* con)
@@ -33,9 +39,18 @@ static void x11_drop(struct arcan_shmif_cont* con)
 if (!con->privext->internal)
 		return;
 
-	glXDestroyContext(con->privext->internal->display,
-		con->privext->internal->ctx);
-	XCloseDisplay(con->privext->internal->display);
+	struct shmif_ext_hidden_int* in = con->privext->internal;
+
+	if (in->managed){
+		glXDestroyContext(in->display, in->ctx);
+		XCloseDisplay(in->display);
+	}
+	if (in->rtgt){
+		agp_drop_rendertarget(in->rtgt);
+		agp_drop_vstore(&in->vstore);
+		in->rtgt = NULL;
+	}
+
 	free(con->privext->internal);
 	con->privext->internal = NULL;
 }
@@ -44,6 +59,11 @@ void* arcan_shmifext_lookup(
 	struct arcan_shmif_cont* con, const char* fun)
 {
 	return glXGetProcAddress((const GLubyte*) fun);
+}
+
+static void* lookup_fun(void* tag, const char* sym, bool req)
+{
+	return glXGetProcAddress((const GLubyte*) sym);
 }
 
 enum shmifext_setup_status arcan_shmifext_setup(
@@ -62,7 +82,7 @@ enum shmifext_setup_status arcan_shmifext_setup(
 	int alist[] = {
 		GLX_RGBA,
 		GLX_DEPTH_SIZE,
-		24,
+		arg.depth,
 		None
 	};
 
@@ -84,10 +104,31 @@ enum shmifext_setup_status arcan_shmifext_setup(
 		return SHMIFEXT_NO_CONTEXT;
 	}
 
+	agp_glinit_fenv(&con->privext->internal->fenv, lookup_fun, NULL);
+
 	if (!glXMakeCurrent(ctx->display, ctx->wnd, ctx->ctx))
 		XSync(ctx->display, False);
 
+	ctx->managed = true;
+	if (arg.builtin_fbo){
+		agp_empty_vstore(&ctx->vstore, con->w, con->h);
+		ctx->rtgt = agp_setup_rendertarget(
+			&ctx->vstore, arg.depth > 0 ? RENDERTARGET_COLOR_DEPTH_STENCIL :
+				RENDERTARGET_COLOR);
+	}
+
 	return SHMIFEXT_OK;
+}
+
+bool arcan_shmifext_gl_handles(struct arcan_shmif_cont* con,
+	uintptr_t* frame, uintptr_t* color, uintptr_t* depth)
+{
+	if (!con || !con->privext || !con->privext->internal ||
+		!con->privext->internal->display || !con->privext->internal->rtgt)
+		return false;
+
+	agp_rendertarget_ids(con->privext->internal->rtgt, frame, color, depth);
+	return true;
 }
 
 bool arcan_shmifext_egl(struct arcan_shmif_cont* con,
@@ -98,7 +139,25 @@ bool arcan_shmifext_egl(struct arcan_shmif_cont* con,
 
 bool arcan_shmifext_make_current(struct arcan_shmif_cont* con)
 {
-	return false;
+	if (!con || !con->privext || !con->privext->internal)
+		return false;
+
+	struct shmif_ext_hidden_int* ctx = con->privext->internal;
+
+	if (!glXMakeCurrent(ctx->display, ctx->wnd, ctx->ctx)){
+		XSync(ctx->display, False);
+		return false;
+	}
+
+	if (ctx->rtgt){
+		if (ctx->vstore.w != con->w || ctx->vstore.h != con->h){
+			agp_activate_rendertarget(NULL);
+			agp_resize_rendertarget(ctx->rtgt, con->w, con->h);
+		}
+		agp_activate_rendertarget(ctx->rtgt);
+	}
+
+	return true;
 }
 
 bool arcan_shmifext_vk(struct arcan_shmif_cont* con,
@@ -110,11 +169,53 @@ bool arcan_shmifext_vk(struct arcan_shmif_cont* con,
 int arcan_shmifext_eglsignal(struct arcan_shmif_cont* con,
 	uintptr_t display, int mask, uintptr_t tex_id, ...)
 {
-	return -1;
+	if (!con || !con->privext || !con->privext->internal)
+		return -1;
+	struct shmif_ext_hidden_int* ctx = con->privext->internal;
+
+	if (tex_id == SHMIFEXT_BUILTIN){
+		if (ctx->managed)
+			tex_id = ctx->vstore.vinf.text.glid;
+		else
+			return -1;
+	}
+
+/*
+ * There are some possible extensions for sharing resources from a GLX
+ * context between processes, but it's on the very distant research- list
+ * and would need a corresponding _map_handle etc. implementation server-side
+ *
+ * right now, juse use the slow readback method
+ */
+	struct storage_info_t vstore = {
+		.w = con->w,
+		.h = con->h,
+		.txmapped = TXSTATE_TEX2D,
+		.vinf.text = {
+			.glid = tex_id,
+			.raw = (void*) con->vidp
+		},
+	};
+
+	if (ctx->rtgt){
+		agp_activate_rendertarget(NULL);
+		agp_readback_synchronous(&vstore);
+		agp_activate_rendertarget(ctx->rtgt);
+	}
+	else
+		agp_readback_synchronous(&vstore);
+
+	unsigned res = arcan_shmif_signal(con, mask);
+	return res > INT_MAX ? INT_MAX : res;
 }
 
 int arcan_shmifext_vksignal(struct arcan_shmif_cont* con,
 	uintptr_t display, int mask, uintptr_t tex_id, ...)
 {
 	return -1;
+}
+
+bool platform_video_map_handle(struct storage_info_t* store, int64_t handle)
+{
+	return false;
 }
