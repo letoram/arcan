58633dfe1f171f24a9a3307f5e8d7b50dc962ca4
arcan-fe@nospam <arcan-fe@nospam>
2016-12-26 01:54:56 +0000

    (hmdbridge) added AHRS implementation
    With the substructure protocol negotiation almost in place, a way of filtering
    MENS sensors was the last piece of the puzzle needed before we can do the USB
    work for hooking up PSVR. The chosen approach was the AHRS algorithm (see code
    for link to paper) rather than going with a kalman filter at this level.
    
    FossilOrigin-Name: c3409d72a41fda2faf00a1b39549484173dc4fb78d925be640b46ad7a9f3fe5c

---
+++ src/tools/hmdbridge/ahrs.h
@@ -0,0 +1,32 @@
+/*
+ * Madgwick AHRS
+ * License (GPLv2)
+ * Reference: http://www.x-io.co.uk/node/8#open_source_ahrs_and_imu_algorithms
+ */
+
+/*
+ * An interesting exploration here would be to use the skeleton model from the
+ * hmd- structure where the mapped joints will have a sensor each, and to feed
+ * this into a Kalman filter that uses a human- tuned inverse-kinematics model
+ * for prediction
+ */
+struct ahrs_context {
+	quat rot_quat;
+	float beta;
+	float rate;
+	float q0, q1, q2, q3;
+};
+
+void AHRS_init(struct ahrs_context*, float samplerate);
+
+/*
+ * Only update if all three sets have been sampled in the same timeframe
+ */
+void AHRS_update(struct ahrs_context*,
+	float gyro[3], float accel[3], float magnet[3]);
+
+/*
+ * Convert the internal ahrs- context to an orientation quaternion suitable for
+ * openGL- use
+ */
+void AHRS_sample(struct ahrs_context*, quat* dst);
