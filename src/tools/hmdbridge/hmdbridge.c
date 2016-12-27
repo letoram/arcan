9315776f14769c59cd4df396c438d56bf32e34fc
arcanfe@nospam <arcanfe@nospam>
2016-12-27 15:51:40 +0000

    (hmdbridge) two files missing from 5712ec63
    FossilOrigin-Name: 2c88649f8b78421aaa2a085b5ebab6f085d7e1b9752a92a5738c6354c570a373

--- src/tools/hmdbridge/hmdbridge.c
+++ src/tools/hmdbridge/hmdbridge.c
@@ -4,45 +4,69 @@
  * Reference: http://arcan-fe.com
  * Description: HMD interfacing tool
  *
- * This tool implements the producer side of the shmif- extension for
- * HMD- related data. The intent is to use it as a prototyping tool for
- * refining the data model, and upon completion, plugin-in whatever
- * VR toolkit is actually usable.
+ * This tool implements the producer side of the shmif- extension for HMD-
+ * related data. Though it currently host the drivers itself, the hope is to be
+ * able to outsource that work and plug-in whatever happens when/if Khronos
+ * manages to clean up the VR framework mess.
  *
- * If possible, we provide not only sensor- data and metadata, but
- * also related devices like audio and video by requesting the relevant
- * subsegments.
+ * If possible, we provide not only sensor- data and metadata, but also related
+ * devices like audio and video by requesting the relevant subsegments.
  *
- * The basic approach is to populate a structure of limbs, where a limb
- * express the object- space coordinates, position and relative to a
- * single avatar.
+ * The basic approach is to populate a structure of limbs, where a limb express
+ * the object- space coordinates, position and relative to a single avatar.
+ * There is a nasty many:many relation between the number of devices and the
+ * number of limbs, with a possibility for overlap.
+ *
+ * Though wasteful, in the case of overlap or redundancies, it is probably
+ * the safest to provide multiple implementations of the same limb, and have
+ * the consumer discard or sensor-fuse.
  */
 #include <arcan_shmif.h>
 #include "arcan_math.h"
 #include <arcan_shmif_sub.h>
 #include <hidapi/hidapi.h>
 #include <pthread.h>
+#include <stdatomic.h>
+#include "hmdbridge.h"
 
-struct runner_meta {
-	struct arcan_shmif_cont* con;
-};
+#ifdef PSVR
+#include "psvr.h"
+#endif
+
+volatile bool shutdown;
+volatile _Atomic uint_least8_t active;
 
 static void* limb_runner(void* arg)
 {
-/*	struct runner_meta* meta = arg; */
-	while (1){
-/* 1. wait for device to update data, update checksum and timestamp */
+	struct dev_ent* meta = arg;
+	while (!shutdown){
+/* 1. sample, and depending on device config, sleep or just spin */
 /* 2. on device lost, flip our allocation bit and exit */
 	}
+
+	if (meta->control)
+		meta->control(meta, SHUTDOWN, 0);
+
 	return NULL;
 }
 
-static void psvr_sampler(struct runner_meta* meta, hid_device* dev)
+static uint64_t ent_alloc_mask;
+static struct dev_ent dev_tbl[] =
 {
+#ifdef PSVR
+	psvr_ent_tbl
+#endif
+};
 
+static bool init_device(struct dev_ent* ent)
+{
+/* run init function, if ok
+ * create mutex and spawn limb_runner thread detached,
+ * forward devent */
+	return false;
 }
 
-static void device_rescan(
+static size_t device_rescan(
 	struct arcan_shmif_cont* cont, struct arcan_shmif_hmd* hmd)
 {
 /*
@@ -50,35 +74,21 @@ static void device_rescan(
  * so the only thing we need to do here is poll the shmif- event-queue and
  * rescan/ realloc if needed.
  */
+	size_t count = 0;
+
+	for (size_t i = 0; i < sizeof(dev_tbl)/sizeof(dev_tbl[0]); i++){
+		if (ent_alloc_mask & (1 << i))
+			continue;
+
+		if (init_device(&dev_tbl[i])){
+			fprintf(stdout, "hmdbridge:rescan() - found %s\n", dev_tbl[i].label);
+			ent_alloc_mask |= 1 << i;
+			count++;
+		}
+	}
+
+	return count;
 }
-
-typedef void(*vr_sampler_fptr)(struct runner_meta*, hid_device* dev);
-typedef void(*vr_init_fptr)(struct runner_meta*, hid_device* dev);
-
-/*
- * The data we're looking for when scanning for new HMD related devices
- * in the USB network. Note that not all set valid meta (hres = 0 is
- * enough to be invalid) because it doesn't match the context or the
- * values cannot be statically defined.
- */
-struct usb_ent {
-	uint16_t vid, pid;
-	char label[16];
-	int limbs;
-	struct hmd_meta meta;
-	vr_init_fptr init;
-	vr_sampler_fptr sampler;
-};
-
-static const struct usb_ent usb_tbl[] =
-{
-	{
-	.vid = 0x54c, .pid = 0x09af, .label = "morpheus",
-	.sampler = psvr_sampler, .limbs = 1,
-	.meta = {
-	}
-	}
-};
 
 int main(int argc, char** argv)
 {
@@ -113,15 +123,37 @@ int main(int argc, char** argv)
  * the entire chain from engine<->hmdbridge<->avatar
  */
 	if (arg_lookup(arg, "test", 0, NULL)){
-
+		fprintf(stderr, "hmdbridge:setup(test), not implemented\n");
+		return EXIT_FAILURE;
 	}
 	else {
-		device_rescan(&con, hmd);
-	}
-
-	arcan_event ev;
-	while (1){
-		while (arcan_shmif_poll(&con, &ev) > 0){
+		while (device_rescan(&con, hmd) == 0){
+			fprintf(stderr, "hmdbridge:setup() - "
+				"no controllers found, sleep/rescan\n");
+			sleep(5);
 		}
 	}
+
+/*
+ * Some HMD devices found, since each device is ran in its own thread we simply
+ * sleep and rescan on request from the server
+ */
+	arcan_event ev;
+	bool running = true;
+	while (running && arcan_shmif_wait(&con, &ev) > 0){
+		if (ev.category == EVENT_TARGET)
+		switch (ev.tgt.kind){
+		case TARGET_COMMAND_EXIT:
+			running = false;
+		break;
+		default:
+		break;
+		}
+	}
+
+	shutdown = true;
+	while (atomic_load(&active) > 0){
+		sleep(1);
+	}
+	return EXIT_SUCCESS;
 }
