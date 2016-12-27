10b421f409cfd434ae83043d46a55e86eee31db1
arcanfe@nospam <arcanfe@nospam>
2016-12-27 21:40:56 +0000

    (shmif/retro) add last_words for common failures
    FossilOrigin-Name: d3e3e331e2b65b8d000410f4efbe4211891c6662dde37baccab377227cb2859d

--- src/shmif/arcan_shmif_control.h
+++ src/shmif/arcan_shmif_control.h
@@ -946,6 +946,6 @@ struct arcan_shmif_page {
  * Short user-readable utf8- message to indicate a possible reason for a
  * crash, failure or otherwise unexpected termination
  */
-	const char last_words[32];
+	char last_words[32];
 };
 #endif
