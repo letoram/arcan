58aa583b82c7b55627da2b41ebd205f69f273e45
arcan-fe@nospam <arcan-fe@nospam>
2017-01-11 19:28:53 +0000

    (lua/doc) aliased away _properties, updated accept_target
    accept_target didn't really match the way it was being used. To avoid missing
    setting an event handler through ref:target_updatehandler, a lua function has
    been added as an optional argument.
    
    Also alised the image_surface_resolve_properties and
    image_surface_initial_properties into the slightly shorter
    image_surface_resolve and image_surface_initial.
    
    FossilOrigin-Name: c20574948f7a2ff5b004564d8b991cc3911b9c883ed92618120d2df7a8e0a14c

--- doc/image_surface_initial.lua
+++ doc/image_surface_initial.lua
@@ -1,9 +1,10 @@
--- image_surface_initial_properties
+-- image_surface_initial
 -- @short: Retrieve a table describing the initial storage state for the specified object.
 -- @inargs: vid
 -- @outargs: proptbl
--- @longdescr: This function aliases image_surface_initial and is kept around
--- for legacy reasons. Please see ref:image_surface_initial instead.
+-- @longdescr: The system distinguishes between transformations that have been applied
+-- after an object was created and the initial state of the object. This function retrieves
+-- the values associated with this initial state and returns it as a property table.
 -- @group: image
 -- @cfunction: getimageinitprop
 -- @related: image_surface_resolve, image_surface_properties
@@ -11,7 +12,7 @@ function main()
 #ifdef MAIN
 	a = load_image("test.png");
 	resize_image(a, 32, 32);
-	iprop = image_surface_initial_properties(a);
+	iprop = image_surface_initial(a);
 	cprop = image_surface_properties(a);
 
 	print(string.format("initial_w: %d, inital_h: %d, current_w: %d, current_h: %d",
