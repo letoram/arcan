3c9e4a2b8dc2e1d9b166c9b08d97753335882a89
arcanfe@nospam <arcanfe@nospam>
2017-02-25 21:35:51 +0000

    (doc) rephrase image_tracetag
    FossilOrigin-Name: dc8884c3ebd1f633ac96f116d59c6baadc72391f9dcf3ce54a3e871c37657ec9

--- doc/image_tracetag.lua
+++ doc/image_tracetag.lua
@@ -1,11 +1,14 @@
 -- image_tracetag
 -- @short: Set or retrieve a tracing tag for the specified video object.
--- @inargs: vobj, *tag*
+-- @inargs: vid, *tag*
 -- @outargs: tag
--- @longdescr: Each video object can have a tracetag associated with it.
--- This tag, intended for debugging purposes and not guaranteed to be
--- present in release builds, will be added to other debug outputs
--- (samples in monitoring mode, or when a debuglevel > 0 is set).
+-- @longdescr:
+-- It is possible to attach a user-defined string to any vid. This tag will
+-- be used in debug/tracing output and crash- dumps in order to more easily
+-- identify various objects, or be used as a custom metadata key for appl-
+-- specific purposes.
+-- If no *tag* is provided, the currently set value will be returned.
+-- If there is no tag set, the returned string will be '(no tag)'.
 -- @group: debug
 -- @cfunction: tracetag
 -- @flags: debugbuild
