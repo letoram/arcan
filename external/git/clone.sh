f86584901360efca34f38b0040284df723e30184
bjornstahl <arcanfe@nospam>
2017-03-04 01:43:50 +0100

    (build) reset external, moved external/openAL-soft
    The clone- uncontrolled repo and manually patch lottery was becoming
    bothersome, especially when there's a need to do more work on the arcan
    backend (we have race conditions in the lwa- version) so switched to
    maintaining the openal- repo at the normal github site and synchronizing
    that one manually instead.

--- external/git/clone.sh
+++ external/git/clone.sh
@@ -11,10 +11,4 @@ DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
 git clone http://luajit.org/git/luajit-2.0.git "$DIR/luajit"
 git clone git://git.sv.nongnu.org/freetype/freetype2.git "$DIR/freetype"
-
-# reason we're doing this bullshit here is to protect against future
-# al breaks, and deal with the bugfest from ExternalProject_Add when it
-# comes to build states, cleanup, patching and so on.
-git clone git://repo.or.cz/openal-soft.git "$DIR/openal"
-git -c user.name="me" -c user.email="me@my.org" -C "$DIR/openal" am -3 --ignore-space-change --ignore-whitespace ../openal.patch
-git -c user.name="me" -c user.email="me@my.org" -C "$DIR/openal" tag patched_al
+git clone https://github.com/letoram/openal.git "$DIR/openal"
