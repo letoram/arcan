7b630191ef66560ff70dba2bb0d48806f5a91c6b
arcanfe@nospam <arcanfe@nospam>
2017-04-27 12:16:21 +0000

    (build) FreeBSD fixes to backlight dependency
    The explicit dependency to backlight fails on most FBSD setups
    that didn't accidentally have most of the linux support installed.
    Added a temporary stub- implementation with the backlight functions
    in place of a sysctl- based version.
    
    FossilOrigin-Name: 5859c1351776e673a319a5c6e5c73376315aa9c49fffa4d5025b13f6f79b4f9b

---
+++ src/platform/stub/libbacklight.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdint.h>
+#include "../egl-dri/libbacklight.h"
+
+long backlight_get_brightness(struct backlight *backlight)
+{
+	return 1;
+}
+
+long backlight_get_max_brightness(struct backlight *backlight)
+{
+	return 1;
+}
+
+long backlight_get_actual_brightness(struct backlight *backlight)
+{
+	return 1;
+}
+
+long backlight_set_brightness(struct backlight *backlight, long brightness)
+{
+	return 0;
+}
+
+void backlight_destroy(struct backlight *backlight)
+{
+}
+
+struct backlight *backlight_init(struct pci_device *dev, int card,
+				 int connector_type, int connector_type_id)
+{
+	return NULL;
+}
