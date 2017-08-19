a8ba39a0efe5d4339c541c3a209903c915446945
bjornstahl <arcanfe@nospam>
2017-08-19 12:05:47 +0200

    (core/lua) add more anchoring points
    The current set of positional anchoring points were somewhat lacking in
    that they only accounted for corners and center, not the edge-midpoints.
    This limits positional options in advanced window schemes. This patch
    adds the missing midpoints.

--- tests/regression/center/center.lua
+++ tests/regression/center/center.lua
@@ -3,33 +3,41 @@ function center(arg)
 -- link some to different spaces so we know the translation works
 	local arb = null_surface(1, 1);
 	local arb2 = null_surface(1, 1);
-	local o = fill_surface(127, 63, 0, 255, 0);
+	local o = fill_surface(127, 127, 0, 255, 0);
 
 -- create one for each possible anchor point
-	local a = fill_surface(16, 32, 255, 0, 0);
-	local b = fill_surface(32, 16, 255, 0, 0);
-	local c = fill_surface(32, 32, 255, 0, 0);
-	local d = fill_surface(32, 32, 255, 0, 0);
-	local e = fill_surface(32, 32, 255, 0, 0);
+	local a = fill_surface(16, 16, 255, 0, 0);
+	local b = fill_surface(16, 16, 255, 0, 0);
+	local c = fill_surface(16, 16, 255, 0, 0);
+	local d = fill_surface(16, 16, 255, 0, 0);
+	local e = fill_surface(16, 16, 255, 0, 0);
+	local f = fill_surface(16, 16, 255, 0, 0);
+	local g = fill_surface(16, 16, 255, 0, 0);
+	local h = fill_surface(16, 16, 255, 0, 0);
+	local i = fill_surface(16, 16, 255, 0, 0);
 
 	move_image(arb, 64, 48);
 	move_image(arb2, 77, 69);
 	link_image(o, arb);
 	link_image(e, arb2);
-	show_image({a,b,c,d,e,o,arb,arb2});
+	show_image({a,b,c,d,e,f,g,h,i,o,arb,arb2});
 
 	center_image(a, o, ANCHOR_UL);
 	center_image(b, o, ANCHOR_LL);
 	center_image(c, o, ANCHOR_LR);
 	center_image(d, o, ANCHOR_UR);
 	center_image(e, o, ANCHOR_C);
+	center_image(f, o, ANCHOR_UC);
+	center_image(g, o, ANCHOR_LC);
+	center_image(h, o, ANCHOR_CL);
+	center_image(i, o, ANCHOR_CR);
 end
 
 clock = 0;
 function center_clock_pulse()
 	clock = clock + 1;
 	if (clock == 25) then
-		save_screenshot(arguments[1]);
-		return shutdown();
+--		save_screenshot(arguments[1]);
+--		return shutdown();
 	end
 end
