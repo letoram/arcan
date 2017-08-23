b26bac407d057127622540deca4a45f6fc508964
bjornstahl <arcan-fe@nospam>
2017-08-23 19:08:52 +0200

    (core) fix use of custom txcos with frameset
    This fixes an issue where the texture coordinates would fail to
    propagate if the dst-vobj and the src-vobj already hade the same
    backing store. A case that would trigger this could look like:
    
    a = fill_surface(64, 64, 255, 0, 0)
    b = null_surface(1, 1)
    image_sharestorage(a, b)
    image_framesetsize(b, 5)
    image_set_txcos(a, {0.5, 0.2, 0.1, 0.2, 0.3, 0.4, 0.1, 0.2})
    set_image_as_frame(b, a, 1)
    
    a would be set as backing store in frameset b at slot 1, but since
    backingstore in a and b already matched, the texture coordinates
    would be damaged rather than copied.

--- doc/image_framecyclemode.lua
+++ doc/image_framecyclemode.lua
@@ -1,15 +1,18 @@
 -- image_framecyclemode
--- @short: Switch how object frameset properties are managed.
--- @inargs: vid, *mode*
--- @longdescr: Framesets associated with an object can have its active_frame
--- property vary with other engine states. By setting *mode* to 0 (disable)
--- only activeframe can alter the active frame property. Otherwise,
--- the active frame will be cycled (increment active index) every n logical
--- frames. For framesets where the containing object, this means that the
--- destination store will change every n new frames instead.
--- @note: If you want the behavior of stepping each rendered frame, do this
--- manually by implementing the applname_frame_pulse and calling
--- image_activeframe rather than relying on a framecyclemode.
+-- @short:  Enable/Disable time-triggered active frameset frame cycling.
+-- @inargs: vid:dst
+-- @inargs: vid:dst, int:time_steps
+-- @longdescr: Every video object with a textured backing store has an
+-- active frame defined, which usually refers to the backing store that
+-- is allocated during object creation or explicitly updated via
+-- ref:image_sharestorage. This frame can also be picked from a larger
+-- set that is allocated via ref:image_framesetsize and populated via
+-- ref:set_image_as_frame. The active frame property can then be
+-- managed manually via ref:image_activeframe or automatically via this
+-- function. If called without any *time_steps* argument, the feature
+-- will be disabled, otherwise the active frame will be cycled in a
+-- (n + 1) % count order at a rate of *time_steps* ticks on the logic
+-- clock.
 -- @group: image
 -- @cfunction: framesetcycle
 -- @related: image_framesetsize, image_activeframe, set_image_as_frame
@@ -22,6 +25,6 @@ function main()
 	image_framesetsize(a, 3);
 	set_image_as_frame(a, b, 1);
 	set_image_as_frame(a, c, 2);
-	image_framecyclemode(a, -1);
+	image_framecyclemode(a, 1);
 #endif
 end
