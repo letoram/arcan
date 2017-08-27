c6474d9beb82ca7cdd14193916de21c409129b92
arcan-fe@nospam <arcan-fe@nospam>
2017-08-27 12:22:34 +0000

    (build/openbsd) compilation fixes
    First signs of life of an openBSD bringup. Biggest missing points right
    now is input (unsurprisingly) that will need something derived from wscons.
    
    Rough hacking needed:
    1. need > 6.1 (so current)
    2. pkg_add on llvm git cmake libelf
    3. clone github.com/letoram/arcan.git -> arcan
    4. clone git://git.sv.nongnu.prg/freetype -> arcan/external/freetype
    4. clone https://guthub.com/letoram/openal.git arcan/external/openal
    
    mkdir arcan/build ; cd arcan/build;
    
    cmake -DBUILTIN_LUA=ON -DSTATIC_SQLITE3=ON -DSTATIC_OPENAL=ON -DVIDEO_PLATFORM=egl-dri -DDISABLE_JIT=ON ../src
    
    make etc.
    should run with:
    
    ARCAN_VIDEO_DEVICE=/dev/drm0 ./arcan -p ../data/resources ../tests/interactive/eventtest
    
    Still quite some things to do, and chances are these patches will need
    to be verified on Free/dragonfly/NetBSD as well.
    
    FossilOrigin-Name: 42d1d1d3e3058fd9ee63f942f2d483c23634e3b116c0b44d34b42ca823dd15da

--- src/platform/cmake/modules/FindGBMKMS.cmake
+++ src/platform/cmake/modules/FindGBMKMS.cmake
@@ -8,15 +8,23 @@ find_path(DRM_INCLUDE_DIR drm.h
     /usr/local/include/drm
     /usr/include/libdrm
     /usr/include/drm
+    /usr/X11R6/include/libdrm
 )
 
 find_path(GBM_INCLUDE_DIR gbm.h
 		PATHS
 		/usr/local/include
 		/usr/include
+                /usr/X11R6/include 
 )
 
-find_library(GBMKMS_DRM_LIBRARY NAMES drm REQUIRED)
+set(GBMKMS_LIB_PATHS
+	/usr/local/lib
+	/usr/lib
+	/usr/X11R6/lib
+)
+
+find_library(GBMKMS_DRM_LIBRARY NAMES drm HINTS ${GBMKMS_LIB_PATHS} REQUIRED)
 
 if (NOT DRM_INCLUDE_DIR OR NOT GBMKMS_DRM_LIBRARY)
 	if (GBMKMS_FIND_REQUIRED)
@@ -25,7 +33,7 @@ if (NOT DRM_INCLUDE_DIR OR NOT GBMKMS_DRM_LIBRARY)
 endif()
 
 if (NOT IGNORE_GBM)
-	find_library(GBMKMS_GBM_LIBRARY NAMES gbm)
+	find_library(GBMKMS_GBM_LIBRARY NAMES gbm HINTS ${GBMKMS_LIB_PATHS})
 	if (NOT GBMKMS_GBM_LIBRARY OR NOT GBM_INCLUDE_DIR)
 		if (GBMKMS_FIND_REQUIRED)
 			message(FATAL_ERROR "Could not find libGBM")
