412dfe448407299796c2f40aa7d88926b5d171c8
bjornstahl <arcan-fe@nospam>
2017-08-29 05:19:14 +0200

    (platform) migrate lwa to use cfg api
    first one up, arcan in arcan build. The ones that will take a bit
    longer is egl-dri/evdev/bsd, but about time that this bandaid got
    removed.

--- src/platform/posix/config.c
+++ src/platform/posix/config.c
@@ -1,7 +1,10 @@
+#include <stdlib.h>
 #include <stdint.h>
 #include <stdbool.h>
 #include <unistd.h>
+#include <stdio.h>
 #include <strings.h>
+#include <ctype.h>
 
 #include "../platform.h"
 
@@ -10,16 +13,26 @@ static uintptr_t token = 0xdeadbabe;
 static bool lookup(const char* const key,
 	unsigned short ind, char** val, uintptr_t tag)
 {
-	if (tag != token || ind > 0){
+	if (!key || tag != token || ind > 0){
 		if (val)
 			*val = NULL;
 		return false;
 	}
 
-/*
- * missing: map to arcan_db with some fallback to ARCAN_xxx env
- */
-	return false;
+	char tmpbuf[strlen(key) + sizeof("ARCAN_")];
+	snprintf(tmpbuf, sizeof(tmpbuf), "%s", key);
+	char* tmp = tmpbuf;
+	while(*tmp){
+		*tmp = toupper(*tmp);
+		tmp++;
+	}
+
+	char* test = getenv(tmpbuf);
+	if (test && val){
+		*val = strdup(test);
+	}
+
+	return test != NULL;
 }
 
 cfg_lookup_fun platform_config_lookup(uintptr_t* tag)
