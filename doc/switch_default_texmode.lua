d2d41a5dcce7ced5d5d70712d3e221e05036ec4c
arcan-fe@nospam <arcan-fe@nospam>
2017-11-02 19:10:57 +0000

    (doc) notes to sharestorage, fix typos
    FossilOrigin-Name: 466862c6ef8513d6bc90f461f7b46d57b8e6b52f4e6cd2d0845e3604b5fe6186

--- doc/switch_default_texmode.lua
+++ doc/switch_default_texmode.lua
@@ -2,7 +2,7 @@
 -- @short: Switch how the associated texture storage should treat coordinates outside the [0..1] range.
 -- @inargs: mode_s, mode_t, *optvid*
 -- @longdescr: If *optvid* isn't set, the global default value for newly allocated textures will be switched.
--- @note: Accepted values for mode_(s,t) are VTEX_REPEAT, VTEX_CLAMP
+-- @note: Accepted values for mode_(s,t) are TEX_REPEAT, TEX_CLAMP
 -- @group: vidsys
 -- @cfunction: settexmode
 
