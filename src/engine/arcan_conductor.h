6232443a5b582b4877feb96024e62328af0cf079
arcanfe@nospam <arcanfe@nospam>
2018-01-08 08:43:02 +0000

    (core) more conductor work and stubbing
    FossilOrigin-Name: ececf06f033f36e1c3d58387959450bf72a65b4cea5e2c9f41a54092188fbdf8

--- src/engine/arcan_conductor.h
+++ src/engine/arcan_conductor.h
@@ -7,18 +7,34 @@
  * strategy to optimize for throughput, latency or energy efficiency.
  */
 
+enum synch_method {
+	SYNCH_NONE = 0,
+	SYNCH_STATIC = 1,
+	SYNCH_DYNAMIC = 2
+};
+
 /* Main processing loop, takes care of invoking the scripting VM, synching
  * display updates and so on. */
 int arcan_conductor_run(arcan_tick_cb cb);
 
-/* Allow processing operations bound to gpu_id to be scheduled, gpu_ids
+/* [ called from platform ]
+ * Specify the relationship between display, gpu, synch and object - displays
+ * are never 'lost' though they can be disabled with a bad vobj. */
+void arcan_conductor_register_display(size_t gpu_id,
+		size_t disp_id, enum synch_method method, float rate, arcan_vobj_id obj);
+
+/* [ called from platform ]
+ * mark GPU as locked and add [fence] to pollset, when there's data on fence,
+ * invoke the lockhandler callback which [may] release the gpu
+ */
+typedef void (*arcan_gpu_lockhandler)(size_t, int);
+void arcan_conductor_lock_gpu(size_t gpu_id, int fence, arcan_gpu_lockhandler);
+
+/* [called from platform ]
+ * Allow processing operations bound to gpu_id to be scheduled, gpu_ids
  * as part of the argument are matched against the bitmask vstore_t used by
  * various producers and consumers */
 void arcan_conductor_release_gpu(size_t gpu_id);
-
-/* Prevent processing operations bound to gpu_id from being scheduled or
- * otherwise modified */
-void arcan_conductor_lock_gpu(size_t gpu_id);
 
 /* Update the priority target to match the specified frameserver. This
  * means that heuristics driving synchronization will be biased towards
