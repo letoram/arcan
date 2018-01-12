f7325003062438505154904fb157a9b9d91f2667
arcanfe@nospam <arcanfe@nospam>
2018-01-12 20:47:01 +0000

    (tui-lua) drop print output, improve color test
    FossilOrigin-Name: c01838ae037ec202d25118fcd25fa4b55db71a299f525dd93c609002319fd779

--- src/tools/ltui/tests/colors.lua
+++ src/tools/ltui/tests/colors.lua
@@ -4,13 +4,8 @@ local function draw(ctx)
 	local iattr = tui_attr(
 		{fr = bg_r, fg = bg_g, fb = bg_b, br = fg_r, bg = fg_g, bb = fg_b}
 	);
-	iattr = tui_attr(
-		{br = 255, bg = 255, bb = 255}
-	);
 
--- if we don't do this, we get interesting visual glitches
--- if we DO do this, we get interesting black frames
---	ctx:erase_screen();
+	ctx:erase_screen();
 	local w, h = ctx:dimensions()
 	for x=0,w-1 do
 		ctx:write_to(x, 0, "S", iattr)
@@ -20,7 +15,28 @@ local function draw(ctx)
 		ctx:write_to(0, y, "S", iattr)
 		ctx:write_to(w-1, y, "S", iattr)
 	end
-	ctx:refresh()
+
+	local set = {};
+	for k,v in pairs(tui_color) do
+		table.insert(set, k)
+	end
+
+	local rowst = ""
+	for i=1,w-2 do
+		rowst = rowst .. " "
+	end
+
+	local ind = 1
+	for y = 1,h-1,2 do
+		iattr.br, iattr.bg, iattr.bb = ctx:get_color(tui_color[set[ind]])
+		ctx:write_to(1, y, rowst, iattr)
+		ctx:write_to(1, y, set[ind], iattr)
+		ind = ind + 1
+		if ind > #set then
+			ind = 1
+		end
+	end
+
 end
 
 local ht = {
@@ -37,7 +53,6 @@ conn:set_handlers(ht);
 conn:set_flags(tui_flags.hide_cursor);
 draw(conn);
 
-while(conn:process()) do
-	conn:refresh();
+while (conn:refresh()) do
+	conn:process();
 end
-
