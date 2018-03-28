4927a2940de0218b972b038753b45d754f1300b6
arcan-fe@nospam <arcan-fe@nospam>
2018-03-28 19:56:04 +0000

    (doc) image_set_txcos/mesh_shader wrong
    FossilOrigin-Name: f1a1f163a9fcf27cc996feea6066c2b2d24e26892e598aa3006092ccf0ac35a7

--- doc/image_set_txcos.lua
+++ doc/image_set_txcos.lua
@@ -1,6 +1,6 @@
 -- image_set_txcos
 -- @short: Override the default texture coordinates for an object.
--- @inargs: vid, ul_s, ul_t, ur_s, ur_t, lr_s, lr_t, ll_s, ll_t
+-- @inargs: vid, txco_t
 -- @longdescr: Some specific effects and tricks (or manually maintaining
 -- spritesheets or similar image packing options) require that texture
 -- coordinates are skewed. This can be achieved either in the vertex shader
@@ -12,6 +12,6 @@ function main()
 #ifdef MAIN
 	a = load_image("test.png");
 	show_image(a);
-	image_set_txcos(a, 0.5, 0.5, 1.0, 0.5, 1.0, 1.0, 1.0, 0.5);
+	image_set_txcos(a, {0.5, 0.5, 1.0, 0.5, 1.0, 1.0, 1.0, 0.5});
 #endif
 end
