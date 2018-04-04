938ef410bc31f442aefc70200ffe94528fec35ed
arcan-fe@nospam <arcan-fe@nospam>
2018-04-04 11:00:33 +0000

    (shmif) SEGID_DEBUG default implementation hook
    This relates to the previous "dynamic loading of TUI" in b3cd666. Normally, the
    DEBUG subsegment comes from an explicit push from the policy layer to the
    client as a means of requesting more data. Not many (= none) actually maps and
    uses this for anything meaningful, and just lets its go to the default
    silent-drop state. This patch prepares for an alternate path where there is a
    default implementation which redirects STDERR to a dynamically loaded TUI
    session and provides inputs for easily preparing / attaching a debugger to the
    source process.
    
    FossilOrigin-Name: 6ece568e313756f2fc35c1ad7b165650efac1c74144d01514511c9eb0f4ed67b

---
+++ src/shmif/arcan_shmif_debugif.c
@@ -0,0 +1,7 @@
+#include "arcan_shmif.h"
+#include "arcan_shmif_debugif.h"
+
+bool arcan_shmif_debugint_spawn(struct arcan_shmif_cont* c)
+{
+	return false;
+}
---
+++ src/shmif/arcan_shmif_debugif.h
@@ -0,0 +1,8 @@
+#ifndef HAVE_SHMIF_DEBUGIF
+/*
+ * Try to spawn / derive a debugging interface pivoting bootstrapped that is
+ * bootstrapped / built using the specified context. Return true if the callee
+ * adopts the context.
+ */
+bool arcan_shmif_debugint_spawn(struct arcan_shmif_cont*);
+#endif
