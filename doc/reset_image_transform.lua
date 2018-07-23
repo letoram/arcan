e171a6713dddec1181172ed0b5d79271f32cc3f6
arcanfe@nospam <arcanfe@nospam>
2018-07-23 07:37:03 +0000

    (core/lua) change return of reset_image_transform
    The previous return value, i.e. the total count of pending transforms
    was wholly useless. Since nothing would / could get any use of that,
    it has been changed to the number of ticks left in each transform slot.
    
    This fills the case where you have a pending animation (single), and an
    event occurs that would prompt the animation to be cancelled and swapped
    for another (think cross-fade between two objects) but you want the
    real-world time elapsed for the initial animation to match.
    
    FossilOrigin-Name: 03fa94750833e475744dd35f14c1a94e5bf87364fe26e6f6972e387458fefddd

--- doc/reset_image_transform.lua
+++ doc/reset_image_transform.lua
@@ -1,18 +1,21 @@
 -- reset_image_transform
 -- @short: Drop all ongoing transformations.
 -- @inargs: vid
--- @outargs: tnum
+-- @outargs: left_blend, left_move, left_rotate
 -- @longdescr: At times there may be queued events that should be
 -- cancelled out due to unforeseen changes, this is especially typical
--- when transformations are initated as part as some input event handler,
--- like pushing a button to move a cursor indicating a selected item. To
--- prevent these from stacking up, reset_image_transformation can first
--- be called to make sure that the transformation queue is empty.
--- Returns the number of dropped transactions, and fractional part
--- represents the completion rate relative to the longest transformation
--- in the first slot.
+-- when transformations are initated as part as some input or client event
+-- handler, like pushing a button to move a cursor indicating a selected item
+-- or invoking a keybinding that triggers new animations. To prevent these
+-- from stacking up, reset_image_transformation can first be called to flush
+-- the queue, and the return values are the number of ticks left to the next
+-- item in the chain before flushing.
+-- These values could then be used to schedule the new transform and make
+-- sure that it would take 'as long' time as the previouslt scheduled one
+-- would have occupied.
 -- @note: this will not revert the object back to a previous state,
--- but rather stop at whatever part of the chain was currently being processed.
+-- but rather stop at whatever part of the chain was currently being
+-- processed.
 -- @group: image
 -- @cfunction: resettransform
 -- @related: instant_image_transform
