19d69755a38e0a817176c70995430efe1ca5419e
bjornstahl <arcanfe@nospam>
2018-12-21 02:28:21 +0100

    (core) switch default synch-mode to adaptive

--- src/engine/arcan_conductor.c
+++ src/engine/arcan_conductor.c
@@ -115,7 +115,7 @@ enum synchopts {
 	SYNCH_TIGHT
 };
 
-static int synchopt = SYNCH_VSYNCH;
+static int synchopt = SYNCH_ADAPTIVE;
 
 /*
  * difference between step/unlock is that step performs a polling step
