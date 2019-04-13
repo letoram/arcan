33b6235aa99cdd5f2286b004e5d5a73313fdf7f1
bjornstahl <arcanfe@nospam>
2019-04-13 22:21:24 +0200

    (core/lua) tag-cb control for instant_transform
    The tag image transform mechanism for getting callback notification on
    completion of a transform stage in transformation chains masks all tags
    by default in the case of an instant image transform on the backing
    object.
    
    This patch adds controls for adjusting if one or all transform tags
    should be processed in such a case.

--- doc/instant_image_transform.lua
+++ doc/instant_image_transform.lua
@@ -1,9 +1,23 @@
 -- instant_image_transform
 -- @short: Immediately perform all pending transformations.
--- @inargs: vid
+-- @inargs: vid:dst
+-- @inargs: vid:dst, bool:trigger_last
+-- @inargs: vid:dst, bool:trigger_last, bool:trigger_all
 -- @group: image
+-- @description:
+-- This will fast-forward through the transform chain for *dst*.
+--
+-- The default behaviour is to ignore all tagged transform handlers. If
+-- *trigger_last* is set, all handlers except the end of each chain will
+-- be ignored. If *trigger_all* is set, all handlers will be triggered.
+--
+-- Both of these behaviours can lead to hard to debug problems due to
+-- the possible storms of feedback loops being created. The safest
+-- solution to this problem has been to simply ignore the handlers,
+-- but that is not always the ideal one.
+--
 -- @cfunction: instanttransform
--- @related: copy_image_transform,
+-- @related: copy_image_transform
 -- image_transform_cycle, reset_image_transform, transfer_image_transform
 function main()
 #ifdef MAIN
