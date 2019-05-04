76639df6b66b4a7be1a0e1f9e1d5b8a4b7db8541
arcanfe@nospam <arcanfe@nospam>
2019-05-04 06:42:21 +0000

    (builtin) shutdown hookscript (hook/shutdown.lua)
    This adds the command-line option for scheduling a shutdown call
    after a certain amount of ticks. The default, 500 ticks, can be
    regulated with an argument:
    
        arcan -H hook/shutdown.lua myappl shutdown=200
    
    The purpose / use for this hook script is primarily for automation,
    development and testing so that this doesn't have to be added to
    every test case.
    
    FossilOrigin-Name: afbb94b588091510bcfe014e2f6d3094dcdeeee4e92a76fde55f1ea9c7ecdaea

---
+++ data/scripts/hook/shutdown.lua
@@ -0,0 +1,28 @@
+local left = 500;
+
+-- not present in older engine versions
+if (appl_arguments) then
+
+	for i,v in ipairs(appl_arguments()) do
+		if string.sub(v, 1, 9) == "shutdown=" then
+			local rem = string.sub(v, 10)
+			local num = tonumber(rem)
+			if num and num > 0 then
+				left = num
+			end
+		end
+	end
+end
+
+warning("shutdown in " .. tostring(left) .. " ticks")
+local old_tick = _G[APPLID .. "_clock_pulse"]
+
+_G[APPLID .. "_clock_pulse"] = function(...)
+	if left > 0 then
+		left = left - 1
+	elseif left == 0 then
+		return shutdown("autoshutdown.lua finished", EXIT_SUCCESS)
+	else
+		old_tick(...)
+	end
+end
