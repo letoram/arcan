d4c2768a78954a8653b610c0317108c750e51cda
bjornstahl <arcanfe@nospam>
2019-05-12 02:14:11 +0200

    (doc) missing fields / notes

--- doc/rotate3d_model.lua
+++ doc/rotate3d_model.lua
@@ -1,11 +1,14 @@
 -- rotate3d_model
 -- @short: Set the current rotation for the specified model.
--- @inargs: model, roll, pitch, yaw, *dt*
+-- @inargs: vid:model, number:roll, number:pitch, number:yaw
+-- @inargs: vid:model, number:roll, number:pitch, number:yaw, int: dt
+-- @inargs: vid:model, number:roll, number:pitch, number:yaw, int: dt, int:mode
 -- @longdescr: The move3d, rotate3d, scale3d etc. class functions are similar to their
 -- 2D counterparts, but takes an additional z coordinate. These could, in fact, be used
 -- on normal 2D objects to achieve other effects as well.
+-- The added *mode* argument can be one of ROTATE_RELATIVE or ROTATE_ABSOLUTE (default)
+-- which affects the interpretation of the angles (= val or += val).
 -- @group: 3d
--- once for any model.
 -- @related: orient3d_model, move3d_model
 -- @cfunction: rotatemodel
 function main()
