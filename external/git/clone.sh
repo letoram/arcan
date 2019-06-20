9a516a704328721a6d566d2baecd387806a76f45
bjornstahl <arcanfe@nospam>
2019-06-20 02:01:01 +0200

    (decode/uvc) switch to fork of libuvc
    Libuvc, though useful compared to the less portable options, seems
    mostly abandoned with years without patches. Since there are quite
    a few that we would want for VR etc. purposes and avoid VLC if all
    possible, it seems fitting to fork it off and patch around a bit.

--- external/git/clone.sh
+++ external/git/clone.sh
@@ -12,4 +12,6 @@ DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 git clone http://luajit.org/git/luajit-2.0.git "$DIR/luajit"
 git clone git://git.sv.nongnu.org/freetype/freetype2.git "$DIR/freetype"
 git clone https://github.com/letoram/openal.git "$DIR/openal"
-git clone https://github.com/ktossell/libuvc.git "$DIR/libuvc"
+
+# we run our own fork of this as ktossel/libuvc is rather dead and numerous patches are missing
+git clone https://github.com/letoram/libuvc.git "$DIR/libuvc"
