9b9ec49c7f4fb48c4519e559a4038c7c8c71d8d5
bjornstahl <arcanfe@nospam>
2019-06-30 03:33:38 +0200

    (doc/lua) disable pid- tracking on auth migrate
    This was a special little edge case, due to the pid- tracking from
    auth mode - migrating trusted clients would block these from sending
    the TERMINATED event, yet they would be time-out killed after
    timeout on the delete_image event.

--- doc/target_devicehint.lua
+++ doc/target_devicehint.lua
@@ -1,27 +1,33 @@
 -- target_devicehint
 -- @short: Change connection- and device- use for a frameserver connection
--- @inargs: fsrvid, target, *mode*
+-- @inargs: vid:dsrv, number:device
+-- @inargs: vid:fsrv, string:target
+-- @inargs: vid:fsrv, string:target, bool:force(=false)
 -- @longdescr: This function serves several uses. The first use case is to
--- indicate to a frameserver or non-authoritative connection that the
--- underlying connection primitive has changed. *mode* is then used as a
--- boolean flag. If *target* is a string or network-fsrv-vid and *mode* is
--- set to true, the *target* should be be used for recovery. This means that
--- if the arcan instances crashes or the connection is lost in some other way,
--- the *target* should be used for recovery/rendez-vous. This is the default.
--- If *mode* is set to false, the target will be hinted that it should migrate
--- to using another connection-point voluntarily, albeit that this is not always
--- possible.
--- Another use case is to provide a handle to an accelerated device (this is
--- platform dependent and typically only available to low-level platforms like
--- egl-dri) to use for hardware accelerated rendering, "render-nodes" in KMS
--- terminology. In this case, *mode* is used as a use-hint flag, and can be
--- either DEVICE_INDIRECT (default), DEVICE_DIRECT or DEVICE_LOST.
--- DEVICE_INDIRECT means that the rendering output will be used for some
--- internal computation or indirect rendering and should use whatever memory
--- type that fits this purpose. DEVICE_LOST and DEVICE_DIRECT are sent as part
--- of other operations, e.g. when the *fsrvid* is directly mapped to an output
--- display. This means that the accelerated buffer output should be of a type
--- that supports being used for scanout.
+-- forward a device handle to an accelerated client. This will happen if the
+-- *device* argument is provided and matches a valid card identifier. If an
+-- invalid identifier is provided, the call will only be interpreted as a
+-- mode hint.
+-- The *mode* argument can also be specified as device-use hint. Previously,
+-- this has been used to indicate of the client should produce a buffer for
+-- direct output on the device in question, but this is now done heuristically
+-- as part of ref:map_video_display calls. The valid values now are
+-- DEVICE_INDIRECT for composed rendering, and DEVICE_LOST to indicate
+-- that the currently used GPU should be dropped and, if possible, revert
+-- to software- defined graphics.
+-- The other uses are with a string target. This is used to indicate a
+-- connection point to another arcan-shmif capable server. The default is
+-- to mark this as a hint, 'in the event of the main connection being lost,
+-- please go here'. If the *force* argument is also set, the connection may
+-- be forcibly severed to make the client switch to the specified connection
+-- point.
+-- The *target* string is assumed to match the valid rules for a connection
+-- point, but if the association is not forced, an empty string is also valid
+-- and will be interpreted as disabling a previously set alternate connection.
+-- If the forced target migration mode is used on a frameserver launched in
+-- an authoritative mode via ref:define_avfeed or similar functions, the
+-- process tracking will be disabled and the kill 'guarantee' on
+-- ref:delete_image will be lost.
 -- @group: targetcontrol
 -- @cfunction: targetdevhint
 -- @flags: experimental
