620dd6a32334a1b1fcab20fd93de491f1ac9cc0a
bjornstahl <arcan-fe@nospam>
2019-07-16 23:12:33 +0200

    (shmif) debug-if continued
    Closing in on the descriptor interception bits

--- src/shmif/arcan_shmif_debugif.c
+++ src/shmif/arcan_shmif_debugif.c
@@ -6,6 +6,7 @@
 #include <ctype.h>
 #include <sys/time.h>
 #include <sys/resource.h>
+#include <sys/stat.h>
 #include <limits.h>
 #include <poll.h>
 
@@ -33,7 +34,32 @@ struct debug_ctx {
 	struct tui_context* tui;
 	int infd;
 	int outfd;
+	bool dead;
 };
+
+static const char* stat_to_str(struct stat* s)
+{
+	if (S_IFIFO & s->st_mode){
+		return "fifo";
+	}
+	else if (S_IFCHR & s->st_mode){
+		return "char";
+	}
+	else if (S_IFDIR & s->st_mode){
+		return "dir";
+	}
+	else if (S_IFBLK & s->st_mode){
+		return "block";
+	}
+	else if (S_IFSOCK & s->st_mode){
+		return "socket";
+	}
+	else if (S_IFREG & s->st_mode){
+		return "false";
+	}
+	else
+		return "unknown";
+}
 
 static void flush_io(struct debug_ctx* ctx)
 {
@@ -53,6 +79,163 @@ static void flush_io(struct debug_ctx* ctx)
 	}
 }
 
+static void run_descriptor(struct debug_ctx* dctx, int fd)
+{
+/* dup- dance, convert to bufferwnd, select- intercept */
+}
+
+static void gen_descriptor_menu(struct debug_ctx* dctx)
+{
+/* build a large pollset, fire it once and see which ones were valid */
+	struct rlimit rlim;
+	int lim = 512;
+	if (0 == getrlimit(RLIMIT_NOFILE, &rlim))
+		lim = rlim.rlim_cur;
+
+/* this is anything but atomic, we run inside an unwitting process, so either
+ * we do something nasty like fork+stop-resume parent on collect or just accept
+ * the race for now - there seem to be no portable solution for this so go with
+ * what we can */
+	struct pollfd* set = malloc(sizeof(struct pollfd) * lim);
+	for (size_t i = 0; i < lim; i++)
+		set[i].fd = i;
+
+	poll(set, lim, 0);
+
+	size_t count = 0;
+	for (size_t i = 0; i < lim; i++){
+		if (!(set[i].revents & POLLNVAL))
+			count++;
+	}
+
+	if (count == 0){
+		free(set);
+		return;
+	}
+	printf("count resolved to %zu\n", count);
+
+/* convert / stat-test the valid descriptors, and build the final menu list */
+	struct tui_list_entry* lents = malloc(sizeof(struct tui_list_entry) * count);
+	if (!lents){
+		free(set);
+		return;
+	}
+
+	struct dent {
+		struct stat stat;
+		int fd;
+	}* dents = malloc(sizeof(struct dent) * count);
+	if (!dents){
+		free(lents);
+		free(set);
+		return;
+	}
+
+	count = 0;
+	for (size_t i = 0; i < lim; i++){
+		if (set[i].revents & POLLNVAL)
+			continue;
+		struct tui_list_entry* lent = &lents[count];
+		lents[count] = (struct tui_list_entry){
+			.tag = count,
+/*		.label = "set later" */
+/*		.attributes =
+ *        CHECKED : already used?
+ *        PASSIVE : couldn't be stat:ed
+ */
+		};
+
+		size_t lbl_len = 256;
+		char* lbl_prefix = malloc(lbl_len);
+		if (!lbl_prefix)
+			continue;
+
+		dents[count].fd = set[i].fd;
+		if (-1 == fstat(dents[count].fd, &dents[count].stat)){
+			lents[count].attributes |= LIST_PASSIVE;
+			snprintf(lbl_prefix, lbl_len, "%d(stat-fail) : %s",
+				set[i].fd, strerror(errno));
+		}
+		else {
+			snprintf(lbl_prefix, lbl_len,	"%d(%s) : can't resolve",
+				set[i].fd, stat_to_str(&dents[count].stat));
+		}
+		lents[count].label = lbl_prefix;
+
+/* resolve to pathname if possible */
+#ifdef F_GETPATH
+#endif
+		count++;
+	}
+
+/* switch to new menu */
+	arcan_tui_listwnd_setup(dctx->tui, lents, count);
+	for(;;){
+		struct tui_process_res res = arcan_tui_process(&dctx->tui, 1, NULL, 0, -1);
+		if (res.errc == TUI_ERRC_OK){
+			if (-1 == arcan_tui_refresh(dctx->tui) && errno == EINVAL)
+				break;
+		}
+
+		struct tui_list_entry* ent;
+		if (arcan_tui_listwnd_status(dctx->tui, &ent)){
+			if (ent){
+/* run descriptor, with ent tag as index */
+				run_descriptor(dctx, dents[ent->tag].fd);
+			}
+			break;
+		}
+	}
+
+	for (size_t i = 0; i < count; i++){
+		free(lents[i].label);
+	}
+
+	free(set);
+	free(lents);
+}
+
+static void root_menu(struct debug_ctx* dctx)
+{
+	struct tui_list_entry menu_root[] = {
+		{
+			.label = "File Descriptors",
+			.attributes = LIST_HAS_SUB,
+			.tag = 0
+		},
+		{
+			.label = "Debugger",
+			.attributes = LIST_HAS_SUB,
+			.tag = 1
+		}
+	};
+
+	while(!dctx->dead){
+		arcan_tui_listwnd_setup(dctx->tui, menu_root, COUNT_OF(menu_root));
+
+		for(;;){
+			struct tui_process_res res =
+				arcan_tui_process(&dctx->tui, 1, NULL, 0, -1);
+
+			if (-1 == arcan_tui_refresh(dctx->tui) && errno == EINVAL)
+				return;
+
+			struct tui_list_entry* ent;
+			if (arcan_tui_listwnd_status(dctx->tui, &ent)){
+
+/* this will just chain into a new listwnd setup, and if they cancel
+ * we can just repeat the setup - until the dead state has been set */
+				if (ent){
+					if (ent->tag == 0)
+						gen_descriptor_menu(dctx);
+				}
+				break;
+			}
+
+		}
+	}
+}
+
 static void* debug_thread(void* thr)
 {
 	struct debug_ctx* dctx = thr;
@@ -63,22 +246,7 @@ static void* debug_thread(void* thr)
 		return NULL;
 	}
 
-	struct tui_list_entry menu_root[] = {
-		{
-			.label = "File Descriptors",
-		},
-		{
-			.label = "Debugger",
-			.tag = 1
-		}
-	};
-	arcan_tui_listwnd_setup(dctx->tui, menu_root, COUNT_OF(menu_root));
-	for(;;){
-		struct tui_process_res res =
-			arcan_tui_process(&dctx->tui, 1, NULL, 0, -1);
-		if (-1 == arcan_tui_refresh(dctx->tui) && errno == EINVAL)
-			break;
-	}
+	root_menu(dctx);
 
 	arcan_tui_destroy(dctx->tui, NULL);
 	free(thr);
