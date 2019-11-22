714ccc67d69511c697891885894b35d6be63151f
bjornstahl <arcanfe@nospam>
2019-11-22 12:45:20 +0100

    (encode) vnc input table translation
    This adds the lookup table translations that got refactored out a
    while back (~0.5) with some quickhacks (blocked symbols) and copying
    the sdl sym translation from the sdl platform. Better solution would
    probably be to suck it up and just use libxkbcommon.

--- src/frameserver/encode/default/vnc.c
+++ src/frameserver/encode/default/vnc.c
@@ -16,6 +16,9 @@
 #include <rfb/rfb.h>
 
 #include <arcan_shmif.h>
+#include <arcan_tuisym.h>
+
+#define DEFINE_XKB
 #include "xsymconv.h"
 
 static struct {
@@ -25,10 +28,14 @@ static struct {
 	int last_x, last_y;
 	int last_mask;
 	struct arcan_shmif_cont shmcont;
+	int client_counter;
 } vncctx = {0};
 
 struct cl_track {
 	unsigned conn_id;
+
+/* modifier mask */
+	bool lshift, rshift, lctrl, rctrl, lalt, ralt, lmeta, rmeta;
 };
 
 static int vnc_btn_to_shmif(int i)
@@ -101,27 +108,157 @@ static void server_pointer (int buttonMask,int x,int y,rfbClientPtr cl)
 	}
 }
 
+/*
+ * copied from src/platform/sdl/event.c
+ */
+static char* to_utf8(uint16_t utf16, uint8_t out[4])
+{
+	int count = 1, ofs = 0;
+	uint32_t mask = 0x800;
+
+	if (utf16 >= 0x80)
+		count++;
+
+	for(size_t i=0; i < 5; i++){
+		if ( (uint32_t) utf16 >= mask )
+			count++;
+
+		mask <<= 5;
+	}
+
+	if (count == 1){
+		out[0] = (char) utf16;
+		out[1] = 0x00;
+	}
+	else {
+		for (int i = (count-1 > 4 ? 4 : count - 1); i >= 0; i--){
+			unsigned char ch = ( utf16 >> (6 * i)) & 0x3f;
+			ch |= 0x80;
+			if (i == count-1)
+				ch |= 0xff << (8-count);
+			out[ofs++] = ch;
+		}
+		out[ofs++] = 0x00;
+	}
+
+	return (char*) out;
+}
+
+/*
+ * in the SDL lower layers these are blocked from presenting as
+ * unicode, but here we get ' ' for many of them instead so need
+ * separate filtering.
+ */
+static bool blocked_sym(uint16_t sym)
+{
+	return (sym &&
+		sym < TUIK_SPACE) || (sym >= TUIK_F1 && sym <= TUIK_F12);
+}
+
+static arcan_event sdl_keydown(
+	uint8_t scancode, uint16_t mod, uint16_t sym, uint16_t unicode)
+{
+	arcan_event newevent = {.category = EVENT_IO};
+
+	newevent.io.datatype = EVENT_IDATATYPE_TRANSLATED;
+	newevent.io.devkind  = EVENT_IDEVKIND_KEYBOARD;
+	newevent.io.input.translated.active = true;
+	newevent.io.input.translated.keysym = sym;
+	newevent.io.input.translated.modifiers = mod;
+	newevent.io.input.translated.scancode = scancode;
+	newevent.io.subid = unicode;
+
+	if (!((mod & (ARKMOD_LCTRL | ARKMOD_RCTRL)) > 0)
+		&& !blocked_sym(sym))
+		to_utf8(unicode, newevent.io.input.translated.utf8);
+
+	return newevent;
+}
+
+static arcan_event sdl_keyup(
+	uint8_t scancode, uint16_t mod, uint16_t sym, uint16_t unicode)
+{
+	arcan_event newevent = {.category = EVENT_IO};
+
+	newevent.io.datatype = EVENT_IDATATYPE_TRANSLATED;
+	newevent.io.devkind  = EVENT_IDEVKIND_KEYBOARD;
+	newevent.io.input.translated.active = false;
+	newevent.io.input.translated.keysym = sym;
+	newevent.io.input.translated.modifiers = mod;
+	newevent.io.input.translated.scancode = scancode;
+	newevent.io.subid = unicode;
+
+	return newevent;
+}
+
+static uint16_t track_modifiers(
+	struct cl_track* cl, bool down, rfbKeySym key)
+{
+/* so why is there a XK_Shift and XKB_KEY, question for the ages */
+	switch(key){
+		case XKB_KEY_Shift_R:
+			cl->rshift = down;
+		break;
+		case XKB_KEY_Shift_L:
+			cl->lshift = down;
+		break;
+		case XKB_KEY_Control_L:
+			cl->lctrl = down;
+		break;
+		case XKB_KEY_Control_R:
+			cl->rctrl = down;
+		break;
+		case XKB_KEY_Alt_R:
+			cl->ralt = down;
+		break;
+		case XKB_KEY_Alt_L:
+			cl->lalt = down;
+		break;
+		case XKB_KEY_Super_R:
+			cl->rmeta = down;
+		break;
+		case XKB_KEY_Super_L:
+			cl->lmeta = down;
+		break;
+		default:
+		break;
+	}
+
+/* then we have the latched modifiers as well (caps lock, num lock, scroll lock) */
+	return
+		(cl->lshift * TUIM_LSHIFT) |
+		(cl->rshift * TUIM_RSHIFT) |
+		(cl->lctrl  * TUIM_LCTRL ) |
+		(cl->rctrl  * TUIM_RCTRL ) |
+		(cl->lalt   * TUIM_LALT  ) |
+		(cl->lalt   * TUIM_LALT  ) |
+		(cl->lmeta  * TUIM_LMETA ) |
+		(cl->rmeta  * TUIM_RMETA);
+}
+
 static void server_key(rfbBool down,rfbKeySym key,rfbClientPtr cl)
 {
 /*
- * A possible option here would be to time and track modifier keys and
- * have a timed safety release etc. in the event of client errors (not
- * uncommon).
+ * since rfb only reasons in X keys, we either need to plug it through
+ * libxkbcommon (platform/evdev has the approach for that, as well as
+ * src/tool/waybridge).
  */
-	arcan_shmif_enqueue(&vncctx.shmcont, &(struct arcan_event){
-		.category = EVENT_IO,
-		.io = {
-			.devid = getpid(),
-			.subid = key,
-			.kind = EVENT_IO_BUTTON,
-			.devkind = EVENT_IDEVKIND_KEYBOARD,
-			.datatype = EVENT_IDATATYPE_TRANSLATED,
-			.input.translated = {
-				.active = down,
-				.keysym = (key < 65536 ? symtbl_in[key] : 0)
-			}
-		}
-	});
+	uint16_t mod = track_modifiers(cl->clientData, down, key);
+
+/*
+ * first convert the rfbKeySym to the corresponding SDL one to mimic
+ * the behaviour in our old sdl video platform
+ */
+	uint8_t scancode = 0;
+	uint16_t sym = symtbl_in[key];
+
+/* just plugged into subid */
+	uint16_t unicode = key;
+
+	struct arcan_event ev = down ?
+		sdl_keydown(scancode, mod, sym, unicode) : sdl_keyup(scancode, mod, sym, unicode);
+
+	arcan_shmif_enqueue(&vncctx.shmcont, &ev);
 }
 
 static void server_dropclient(rfbClientPtr cl)
@@ -153,6 +290,11 @@ static void vnc_serv_deltaupd()
  */
 	struct arcan_shmif_region dirty;
 	dirty = atomic_load(&vncctx.shmcont.addr->dirty);
+	if (dirty.x2 < vncctx.shmcont.w)
+		dirty.x2++;
+	if (dirty.y2 < vncctx.shmcont.h)
+		dirty.y2++;
+
 	rfbMarkRectAsModified(vncctx.server, dirty.x1, dirty.y1, dirty.x2, dirty.y2);
 	vncctx.shmcont.addr->vready = false;
 }
