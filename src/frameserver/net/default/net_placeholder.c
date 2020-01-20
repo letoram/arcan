773175e065d24c2cff8fc1e215c6d49d9dc0a988
arcanfe@nospam <arcanfe@nospam>
2020-01-20 17:38:50 +0000

    (build/fsrv-net) drop fsrv-net implementation
    The previous net implementation was mostly an experiment to grok the Lua
    API boundary and requirements. For the 0.6 series, this will be remade
    to actually deal with the directory- server/ discovery layer, likely by
    settling on one of the interesting 'future net' like solutions for this
    specific task, and then use the a12 implementation for the actual
    communication and security.
    
    FossilOrigin-Name: ed233d69d4c130ba398b3fca6373202cf507bcd38d2ec0a7ecb9bd15a5fa4a2f

---
+++ src/frameserver/net/default/net_placeholder.c
@@ -0,0 +1,11 @@
+#include <arcan_shmif.h>
+
+int afsrv_netcl(struct arcan_shmif_cont* c, struct arg_arr* args)
+{
+	return EXIT_FAILURE;
+}
+
+int afsrv_netsrv(struct arcan_shmif_cont* c, struct arg_arr* args)
+{
+	return EXIT_FAILURE;
+}
