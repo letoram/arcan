e284b361ad4ac285f07e94405c15c6ad9d060ed8
arcanfe@nospam <arcanfe@nospam>
2020-12-23 19:27:36 +0000

    (build/afsrv_net) start resurrecting afsrv_net
    Now that the a12 libraries and support code (keystore etc.) is in place,
    time to rework afsrv_net to use it. First up is the net_listen case where
    the primary segment is used as a control and authentication plane, and
    handover segments then feed into arcan-net.
    
    FossilOrigin-Name: 623f8b3353ad3fef7213c41c428f15f1138e7199ff3aaed5f4212105b96fa3f3

--- src/frameserver/net/default/net_placeholder.c
+++ src/frameserver/net/default/net_placeholder.c
@@ -1,4 +1,11 @@
 #include <arcan_shmif.h>
+#include <arcan_shmif_server.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include "a12.h"
+#include "../../util/anet_helper.h"
 
 int afsrv_netcl(struct arcan_shmif_cont* c, struct arg_arr* args)
 {
@@ -7,5 +14,11 @@ int afsrv_netcl(struct arcan_shmif_cont* c, struct arg_arr* args)
 
 int afsrv_netsrv(struct arcan_shmif_cont* c, struct arg_arr* args)
 {
+/* wait for incoming connection,
+ * authenticate
+ * run threaded (!) <-
+ * this should be switch to handover exec to ourselves where we pass
+ * the session key post-auth
+ */
 	return EXIT_FAILURE;
 }
