4e35d84881634cbc88df0f5079debca04fc37d9d
bjornstahl <arcanfe@nospam>
2021-02-27 17:34:44 +0100

    (doc) missed case for video_synchronization

--- doc/video_synchronization.lua
+++ doc/video_synchronization.lua
@@ -1,15 +1,16 @@
 -- video_synchronization
 -- @short: Get or set the current synchronization strategy
 -- @inargs:
+-- @inargs: string:strategy
 -- @outargs: nil or strtbl
 -- @longdescr: There are two stages for controlling synchronization strategy.
 -- The first one is static and can only be formed on the command-line, which should
 -- be limited to strategies that require specific video platform configuration that
--- could invalidate the current graphics context. The other stage can be controlled
--- using this function. First a call with a nil *stratname* argument which will
--- return a table with the set of allowed values. One of these values can then be
--- used in a subsequent call which will switch the active strategy to the one
--- specified, if possible.
+-- could invalidate the current graphics context. The other stage can be
+-- controlled using this function. First as a call with no arguments which will
+-- return a table with the set of allowed values. One of these values can then
+-- be used in a subsequent call which will switch the active strategy to the
+-- one specified, if possible.
 -- @note: The resulting *strtbl* values are valid only until the next call to
 -- set a video_synchronization.
 -- @note: Attempting to set an invalid or unknown synchronization will silently
