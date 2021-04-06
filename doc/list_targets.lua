e9a5de44b683a42961a6837c04fbb5d1da46783d
arcanfe@nospam <arcanfe@nospam>
2021-04-06 16:21:58 +0000

    (doc) list_targets typo
    FossilOrigin-Name: 31f9194b6c491142778abd6ec78b782a00df77e0463c86000ebb393771f29fc0

--- doc/list_targets.lua
+++ doc/list_targets.lua
@@ -4,7 +4,7 @@
 -- @longdescr: Query the database for all targets in the current database,
 -- or all targets in the database with a tag that match the optional *tag*.
 -- @outargs: targettbl
--- @related: target_configures, list_target_tags
+-- @related: target_configurations, list_target_tags
 -- @group: database
 -- @cfunction: gettargets
 function main()
