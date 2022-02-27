cbd2bbc00aba24311e12f47ecc7d96ad0004bbff
arcanfe@nospam <arcanfe@nospam>
2022-02-27 21:04:16 +0000

    (lua) open_rawresource regression
    This >should< fix the spinlock/invalid shaders in durden. The
    problem was the nbio refactoring going on as part of the lua-tui
    work to better accomodate asynch-like behaviour.
    The wrong EOF management code had been commited, causing the
    read_rawresource - to never fail and (upon fixing) return lines
    with different linefeed behaviour than before.
    That being said, the support scripts and durden should move away
    from rawresource_ class functions so they can be removed (deprecated
    for a long time) and just get a .lua fixup when needed.
    
    FossilOrigin-Name: f8e4524436076b47126864cc2578b00c503691a6d8f6b147bab44f3ac70a5fce

--- src/engine/alt/nbio.c
+++ src/engine/alt/nbio.c
@@ -471,18 +471,7 @@ int alt_nbio_process_read(
 	ssize_t nr = read(ib->fd, &ib->buf[ib->ofs], buf_sz - ib->ofs);
 
 	if (0 == nr){
-		struct pollfd fd = {
-			.fd = ib->fd,
-			.events = POLLERR | POLLHUP | POLLNVAL
-		};
-		if (1 == poll(&fd, 1, 0) && fd.revents){
-			eof = true;
-		}
-		else {
-			lua_pushnil(L);
-			lua_pushboolean(L, true);
-			return 2;
-		}
+		eof = true;
 	}
 	else if (-1 == nr){
 		if (errno == EAGAIN || errno == EINTR){
