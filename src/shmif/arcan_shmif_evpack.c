18e121bdc1f80a599ad03551b512635a20758d90
bjornstahl <arcanfe@nospam>
2022-03-26 13:31:57 +0000

    (shmif) exec dup:ed wrong entries to stdio
    the extended / reworked form of exec_pipe was duping in the wrong
    direction, causing parent stdio to persist.

--- src/shmif/arcan_shmif_evpack.c
+++ src/shmif/arcan_shmif_evpack.c
@@ -297,8 +297,8 @@ const char* arcan_shmif_eventstr(arcan_event* aev, char* dbuf, size_t dsz)
 				ev.tgt.ioevs[0].iv, ev.tgt.ioevs[1].iv);
 		break;
 		case TARGET_COMMAND_NEWSEGMENT:
-			snprintf(work, dsz,"TGT:NEWSEGMENT(cookie:%d, direction: %s, type: %d)",
-				ev.tgt.ioevs[3].iv, ev.tgt.ioevs[1].iv ? "read" : "write",
+			snprintf(work, dsz,"TGT:NEWSEGMENT(cookie:%u, direction: %s, type: %d)",
+				ev.tgt.ioevs[3].uiv, ev.tgt.ioevs[1].iv ? "read" : "write",
 				ev.tgt.ioevs[2].iv);
 		break;
 		case TARGET_COMMAND_REQFAIL:
