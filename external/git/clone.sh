bd43fdd76cf50341775f8ec08e4165fab8890cd8
arcanfe@nospam <arcanfe@nospam>
2022-04-14 04:12:02 +0000

    (build) evict vendored ttf
    This was a long time coming, though there are a step or two left - the
    culprit is mainly shmif-tui which strictly doesn't need to know much
    about the fonts except which glyphs are available or when ligatures are
    supported and the GSUB tables need to be processed. A possible 'way out'
    would be to eventually invent a compacted meta format that only contain
    this information.
    
    relatedly - the modified arcan_ttf.c (comes from SDL_ttf) is starting to
    show its age and most of the features we added have been included in
    upstream SDL (although it has grown to twice its current size).
    
    FossilOrigin-Name: 9ac050bf6c3ae8682947f04b67eafd1fbbabe0182048d78b80b6bea47a3230bf

--- external/git/clone.sh
+++ external/git/clone.sh
@@ -10,6 +10,5 @@ done
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
 git clone http://luajit.org/git/luajit-2.0.git "$DIR/luajit"
-git clone git://git.sv.nongnu.org/freetype/freetype2.git "$DIR/freetype"
 git clone https://github.com/letoram/openal.git "$DIR/openal"
 git clone https://github.com/libuvc/libuvc.git "$DIR/libuvc"
