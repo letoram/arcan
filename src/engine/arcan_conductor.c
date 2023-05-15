4051e322ece94b1a01cec4310ed5aebf10114e56
cipharius <cipharius@pm.me>
2023-05-15 15:20:41 +0300

    Integrates Tracy client
    This provides a Tracy client integration which hooks into the existing arcan tracing facilities.
    Effectively that means the Tracy client specific code can be kept mainly confined in the arcan_trace.c, but with slightly more performance overhead compared to directly using Tracy API for marking execution zones.
    
    As a side effect to this integration the CMake platform lists had to be refactored.
    During hybrid platform build phase, the C definitions got wiped, so platform specific build flags had to be seperated in order to keep global build flags intact.

--- src/engine/arcan_conductor.c
+++ src/engine/arcan_conductor.c
@@ -574,6 +574,10 @@ static int trigger_video_synch(float frag)
 	TRACE_MARK_ENTER("conductor", "platform-frame", TRACE_SYS_DEFAULT, conductor.tick_count, frag, "");
 		arcan_lua_callvoidfun(main_lua_context, "preframe_pulse", false, NULL);
 			platform_video_synch(conductor.tick_count, frag, NULL, NULL);
+
+			#ifdef WITH_TRACY
+			TracyCFrameMark
+			#endif
 		arcan_lua_callvoidfun(main_lua_context, "postframe_pulse", false, NULL);
 	TRACE_MARK_EXIT("conductor", "platform-frame", TRACE_SYS_DEFAULT, conductor.tick_count, frag, "");
 
