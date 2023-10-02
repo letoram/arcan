1bde0e30d0a8fa15d24df83aec7a1369a6c29eb1
bjornstahl <arcanfe@nospam>
2023-10-02 02:01:11 +0200

    (a12) refactor dircl to handle bg processing
    This changes the dircl runner around to keep processing a12 packets
    while running an appl. While the monitoring channel still is primitive
    it should be enough for moving on with live-reloading appls on update.
    
    The refactor also allows for running multiple appls simultaneously over
    the same a12 connection, although it is not permitted / enabled yet as
    it makes other parts of the stack more difficult to develop right now.
    
    It should also be a much smaller step bootstrapping shmifsrv over the
    monitoring channel and using that to better integrate the appl with the
    a12 connection. This one is important as the last hurdle before adding
    the controller layer to the directory server, more extensive state synch
    and appl-appl messaging.

--- src/a12/net/dir_supp.c
+++ src/a12/net/dir_supp.c
@@ -29,47 +29,44 @@
 #include <fcntl.h>
 #include <poll.h>
 
-bool g_shutdown;
-
-void anet_directory_ioloop
-	(struct a12_state* S, void* tag,
-	int fdin, int fdout,
-	int usrfd,
-	void (*on_event)(struct arcan_shmif_cont* cont, int chid, struct arcan_event*, void*),
-	bool (*on_directory)(struct a12_state* S, struct appl_meta* dir, void*),
-	void (*on_userfd)(struct a12_state* S, void*))
+void anet_directory_ioloop(struct ioloop_shared* I)
 {
 	int errmask = POLLERR | POLLNVAL | POLLHUP;
 	struct pollfd fds[3] =
 	{
-		{.fd = usrfd, .events = POLLIN | errmask},
-		{.fd = fdin, .events = POLLIN | errmask},
-		{.fd = -fdout, .events = POLLOUT | errmask}
+		{.fd = I->userfd, .events = POLLIN | errmask},
+		{.fd = I->fdin, .events = POLLIN | errmask},
+		{.fd = -1, .events = POLLOUT | errmask}
 	};
 
 	uint8_t inbuf[9000];
 	uint8_t* outbuf = NULL;
 	uint64_t ts = 0;
 
-	fcntl(fdin, F_SETFD, FD_CLOEXEC);
-	fcntl(fdout, F_SETFD, FD_CLOEXEC);
+	fcntl(I->fdin, F_SETFD, FD_CLOEXEC);
+	fcntl(I->fdout, F_SETFD, FD_CLOEXEC);
 
-	size_t outbuf_sz = a12_flush(S, &outbuf, A12_FLUSH_ALL);
+	size_t outbuf_sz = a12_flush(I->S, &outbuf, A12_FLUSH_ALL);
 
 	if (outbuf_sz)
-		fds[2].fd = fdout;
+		fds[2].fd = I->fdout;
 
 /* regular simple processing loop, wait for DIRECTORY-LIST command */
-	while (a12_ok(S) && -1 != poll(fds, 3, -1)){
-		if ((fds[0].revents | fds[1].revents | fds[2].revents) & errmask)
+	while (a12_ok(I->S) && -1 != poll(fds, 3, -1)){
+		if ((fds[0].revents | fds[1].revents | fds[2].revents) & errmask){
+			if (fds[0].revents & errmask){
+				I->on_userfd(I, false);
+			}
+
 			break;
+		}
 
 		if (fds[0].revents & POLLIN){
-			on_userfd(S, tag);
+			I->on_userfd(I, true);
 		}
 
 		if ((fds[2].revents & POLLOUT) && outbuf_sz){
-			ssize_t nw = write(fdout, outbuf, outbuf_sz);
+			ssize_t nw = write(I->fdout, outbuf, outbuf_sz);
 			if (nw > 0){
 				outbuf += nw;
 				outbuf_sz -= nw;
@@ -77,7 +74,7 @@ void anet_directory_ioloop
 		}
 
 		if (fds[1].revents & POLLIN){
-			ssize_t nr = recv(fdin, inbuf, 9000, 0);
+			ssize_t nr = recv(I->fdin, inbuf, 9000, 0);
 			if (-1 == nr && errno != EAGAIN && errno != EWOULDBLOCK && errno != EINTR){
 				a12int_trace(A12_TRACE_DIRECTORY, "shutdown:reason=rw_error");
 				break;
@@ -86,29 +83,29 @@ void anet_directory_ioloop
 				a12int_trace(A12_TRACE_DIRECTORY, "shutdown:reason=closed");
 				break;
 			}
-			a12_unpack(S, inbuf, nr, tag, on_event);
+			a12_unpack(I->S, inbuf, nr, I, I->on_event);
 
 /* check if there has been a change to the directory state after each unpack */
 			uint64_t new_ts;
-			if (on_directory){
-				struct appl_meta* dir = a12int_get_directory(S, &new_ts);
+			if (I->on_directory){
+				struct appl_meta* dir = a12int_get_directory(I->S, &new_ts);
 				if (new_ts != ts){
 					ts = new_ts;
-					if (!on_directory(S, dir, tag))
+					if (!I->on_directory(I, dir))
 						return;
 				}
 			}
 		}
 
 		if (!outbuf_sz){
-			outbuf_sz = a12_flush(S, &outbuf, A12_FLUSH_ALL);
-			if (!outbuf_sz && g_shutdown){
+			outbuf_sz = a12_flush(I->S, &outbuf, A12_FLUSH_ALL);
+			if (!outbuf_sz && I->shutdown)
 				break;
-			}
 		}
 
 		fds[0].revents = fds[1].revents = fds[2].revents = 0;
-		fds[2].fd = outbuf_sz ? fdout : -1;
+		fds[2].fd = outbuf_sz ? I->fdout : -1;
+		fds[0].fd = I->userfd;
 	}
 }
 
